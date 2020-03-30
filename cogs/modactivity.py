27efb4378278db3e31de65924614d53e84827c8a
unknown <yagomichalak@gmail.com>
2020-03-30 11:53:45 -0300

    sloth

--- cogs/modactivity.py
+++ cogs/modactivity.py
@@ -18,7 +18,22 @@ class ModActivity(commands.Cog):
         print('ModActivity cog is ready!')
 
     @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        guild = self.client.get_guild(guild_id)
+        moderator_role = discord.utils.get(guild.roles, id=mod_role_id)
+        if not moderator_role in message.author.roles:
+            return
+
+        await self.get_moderator_current_messages(message.author.id)
+        await self.update_moderator_message(message.author.id)
+
+
+    @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
+        if member.bot:
+            return
         guild = self.client.get_guild(guild_id)
         moderator_role = discord.utils.get(guild.roles, id=mod_role_id)
         if not moderator_role in member.roles:
@@ -26,7 +41,7 @@ class ModActivity(commands.Cog):
         epoch = datetime.utcfromtimestamp(0)
         the_time = (datetime.utcnow() - epoch).total_seconds()
         old_time = await self.get_moderator_current_timestamp(member.id, int(the_time))
-        addition = int(the_time) - old_time
+        addition = the_time - old_time
 
         if not before.channel:
             return await self.update_moderator(member.id)
@@ -58,29 +73,65 @@ class ModActivity(commands.Cog):
             await self.insert_moderator(mod_id, old_ts)
             return await self.get_moderator_current_timestamp(mod_id, old_ts)
 
-        return mod[0][2]
+        if mod[0][2]:
+            return mod[0][2]
+        else:
+            return old_ts
 
     async def insert_moderator(self, mod_id: int, old_ts: int):
         mycursor, db = await the_data_base()
-        await mycursor.execute("INSERT INTO ModActivity (mod_id, time, timestamp) VALUES (%s, %s, %s)",
-                               (mod_id, 0, old_ts))
+        await mycursor.execute("INSERT INTO ModActivity (mod_id, time, timestamp, messages) VALUES (%s, %s, %s, %s)",
+                               (mod_id, 0, old_ts, 0))
+        await db.commit()
+        await mycursor.close()
+
+    async def get_moderator_current_messages(self, mod_id: int):
+        mycursor, db = await the_data_base()
+        await mycursor.execute(f'SELECT * FROM ModActivity WHERE mod_id = {mod_id}')
+        mod = await mycursor.fetchall()
+        await mycursor.close()
+        if not mod:
+            await self.insert_moderator_message(mod_id)
+            return await self.get_moderator_current_messages(mod_id)
+
+        return mod[0][3]
+
+    async def insert_moderator_message(self, mod_id: int):
+        mycursor, db = await the_data_base()
+        await mycursor.execute("INSERT INTO ModActivity (mod_id, time, timestamp, messages) VALUES (%s, %s, %s, %s)",
+                               (mod_id, 0, None, 1))
+        await db.commit()
+        await mycursor.close()
+
+    async def update_moderator_message(self, mod_id: int):
+        mycursor, db = await the_data_base()
+        await mycursor.execute(f'UPDATE ModActivity SET messages = messages+1 WHERE mod_id = {mod_id}')
         await db.commit()
         await mycursor.close()
 
     @commands.has_permissions(administrator=True)
     @commands.command()
-    async def showall(self, ctx):
+    async def modrep(self, ctx):
         mycursor, db = await the_data_base()
         await mycursor.execute('SELECT * FROM ModActivity')
         mods = await mycursor.fetchall()
         await mycursor.close()
-        embed = discord.Embed(title="Weekly Moderation Activity Log",
-                              description="Time spent by the mods in Voice-Channels.",
+        embed = discord.Embed(title="This Week's Report",
+                              description="Moderators' activity statuses.",
+                              url='https://discordapp.com',
                               colour=discord.Colour.dark_green(), timestamp=ctx.message.created_at)
-        embed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url)
+        embed.set_footer(text='Activity Report',
+                         icon_url=self.client.user.avatar_url)
+        embed.set_thumbnail(
+            url='https://cdn.discordapp.com/icons/459195345419763713/a_866e6fe442bfb651353df72826238f54.jpg')
+        embed.set_author(name=ctx.guild.name)
         for mod in mods:
+            m, s = divmod(mod[1], 60)
+            h, m = divmod(m, 60)
             user = discord.utils.get(ctx.guild.members, id=mod[0])
-            embed.add_field(name=user, value=f"Hours spent: {((mod[1]) / 3600):.2f}", inline=False)
+            embed.add_field(name=f'👤__**{user}**__',
+                            value=f"**- Voice Chat Activity:**\n{h:d} hours, {m:02d} minutes and {s:02d} seconds\n**- Text Chat Activity:**\n{mod[3]} messages",
+                            inline=False)
 
         await ctx.send(embed=embed)
         the_msg = await ctx.send("**Do you want to reset the data?**")
@@ -110,12 +161,15 @@ class ModActivity(commands.Cog):
 
     # Database commands
     @commands.command()
+    @commands.has_permissions(administrator=True)
     async def create_mod_table(self, ctx):
         mycursor, db = await the_data_base()
-        await mycursor.execute('CREATE TABLE ModActivity (mod_id bigint, time bigint, timestamp bigint)')
+        await mycursor.execute(
+            'CREATE TABLE ModActivity (mod_id bigint, time bigint, timestamp bigint default null, messages int)')
         await mycursor.close()
 
     @commands.command()
+    @commands.has_permissions(administrator=True)
     async def drop_table_mods(self, ctx):
         mycursor, db = await the_data_base()
         await mycursor.execute('DROP TABLE ModActivity')
