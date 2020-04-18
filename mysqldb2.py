784d3645a64f5f73a1ec1e0b54db9626c63941fa
Yago <yagomichalak@gmail.com>
2020-04-18 20:46:27 -0300

    Sloth

--- mysqldb2.py
+++ mysqldb2.py
@@ -13,3 +13,17 @@ async def the_data_base2():
 
     mycursor = await db.cursor()
     return mycursor, db
+
+
+loop2 = asyncio.get_event_loop()
+
+#mysql://b0df23b6f3cd0c:4feaafce@us-cdbr-iron-east-01.cleardb.net/heroku_6452134eab38dfc?reconnect=true
+
+async def the_data_base3():
+    db = await aiomysql.connect(host='us-cdbr-iron-east-01.cleardb.net',
+                                user='b0df23b6f3cd0c',
+                                password='4feaafce',
+                                db='heroku_6452134eab38dfc', loop=loop2)
+
+    mycursor = await db.cursor()
+    return mycursor, db
