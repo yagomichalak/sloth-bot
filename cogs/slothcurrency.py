4b17e92cd0f322f8182262d43b0176d3072da168
Yago <yagomichalak@gmail.com>
2020-04-22 10:28:43 -0300

    sloth

--- cogs/slothcurrency.py
+++ cogs/slothcurrency.py
@@ -82,10 +82,17 @@ class SlothCurrency(commands.Cog):
             await self.update_user_server_time(member.id, addition)
 
     @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload):
+    async def on_raw_reaction_add(self, payload):        
         # Checks if it wasn't a bot's reaction
         if payload.member.bot:
             return
+
+        guild = self.client.get_guild(payload.guild_id)
+        channel = discord.utils.get(guild.channels, id=payload.channel_id)
+        msg = await channel.fetch_message(payload.message_id)
+        member = discord.utils.get(guild.members, id=payload.user_id)
+        await msg.remove_reaction(payload.emoji.name, member)        
+
         # Checks if it was a reaction within the shop's channel
         if payload.channel_id != shop_channel_id:
             return
