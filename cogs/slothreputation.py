d9d66e5897484492de598c226855a4f270134a5d
Yago <yagomichalak@gmail.com>
2020-05-07 16:01:12 -0300

    sloth

--- cogs/slothreputation.py
+++ cogs/slothreputation.py
@@ -82,7 +82,7 @@ class SlothReputation(commands.Cog):
         embed = discord.Embed(title="Info", colour=member.color, timestamp=ctx.message.created_at)
         embed.add_field(name="__**Level**__", value=f"{user[0][2]}.", inline=True)
         embed.add_field(name="__**EXP**__", value=f"{user[0][1]} / {((user[0][2]+1)**5)}.", inline=True)
-        embed.add_field(name="__**Participated in**__", value=f"{ucur[0][3]} classes.", inline=True)
+        embed.add_field(name="__**Participated in**__", value=f"{ucur[0][3]} classes.", inline=False)
         embed.add_field(name="__**Rewarded in**__", value=f"{ucur[0][4]} classes.", inline=True)
         embed.add_field(name="__**Hosted**__", value=f"{ucur[0][5]} classes.", inline=True)
         embed.set_footer(text=f"{member}", icon_url=member.avatar_url)
