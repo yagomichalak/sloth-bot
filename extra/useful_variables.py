bef98f4ee3f3a2ff787867c1c04db50c04858a57
Yago <yagomichalak@gmail.com>
2020-07-06 16:05:37 -0300

    bot

---
+++ extra/useful_variables.py
@@ -0,0 +1,43 @@
+list_of_commands = [
+['• Moderation (8€)',
+'''
+`|Snipe|` - Gets the last deleted message sent in the server.
+`|Purge|` - Deletes a given amount of messages
+`|Warn|` - Warns a member.
+`|Mute|` - Mutes a member.
+`|Unmute|` - Unmutes a member.
+`|Tempmute|` - Temporarily mutes a member
+`|Ban|` - Bans a member.
+`|Unban|` - Unbans a member.
+`|Softban|` - Softbans a member.
+`|Hackban|` - Bans a member that is currently not in your server.
+`|Logs|` - Moderation commands will be logged in a specific channel'''],
+['• Social (3€)',
+'''
+`|Serverinfo|` - Shows some information about the server.
+`|Userinfo|` - Shows some information about a member.
+`|Meme|` - Fetchs a random meme from the meme subReddit.'''],
+['• General (5€)',
+'''
+`|Create a Room|` - Allows the member to join a specific voice channel and automatically create a voice channel; or even asking the member about the room configurations in their dm's.
+`|modrep|` - The bot sends an embedded message showing the activity of the moderators; allowing you to erase the statuses so it can start counting it again.'''],
+['• Currency system (7€)',
+'''
+`|points|` - A system that will give points/coins to users based on ther activity on the server; such as by sending X messages or having spent some time in the voice channels.
+`|shop|` - A menu showing items that are avaiable for purchase by using those coins. (Roles are a good option for items)'''],
+['• Tool (8€)',
+'''
+`|count|` - Countsdown by given numerber.
+`|tts|` - A text-to-speech command.
+`|tr|` - Translates a message to another language, by specifying the initials of language. Ex: en for English or es for spanish.
+`|ping|` - Shows the bot's ping.
+`|math|` - Math operation commands; addition, subtraction, division, multiplication etc.
+`|custom help command|` - A neat custom help command, that shows all the bot's available commands with their descriptions and usage.
+`|members|` - Shows the current amount of members in the server.
+`|status|` - The bot will show something in its status; it can be playing, streaming, watching or listening to something.'''],
+['• Eval (4€)',
+'''A powerful command that basically allows you to do most Python commands from within Discord.'''],
+['• Custom commands (??)',
+'''
+I'm open to new ideas, in other words, if you want a custom command designed specially for your server, having database access or not, you tell me how you want it to be done and if it's doable, I'll do it.''']
+]
