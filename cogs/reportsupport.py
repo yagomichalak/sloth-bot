e04faef35f76e7f17771ff701ce1f06403471e00
Yago <yagomichalak@gmail.com>
2020-10-15 13:02:24 -0300

    added interview rooms for people who applied to teacher

--- cogs/reportsupport.py
+++ cogs/reportsupport.py
@@ -1,40 +1,63 @@
 import discord
 from discord.ext import commands
-from mysqldb2 import the_data_base3
+from mysqldb2 import the_data_base3, the_data_base5
 import asyncio
 from extra.useful_variables import list_of_commands
 import time
+from typing import List, Dict
 
 reportsupport_channel_id = 729454413290143774
 dnk_id = 647452832852869120
 case_cat_id = 562939721253126146
 moderator_role_id = 497522510212890655
+admin_role_id = 574265899801116673
 
 class ReportSupport(commands.Cog):
 	'''
 	A cog related to the system of reports and some other things.
 	'''
 
-	def __init__(self, client):
+	def __init__(self, client) -> None:
 		self.client = client
-		self.app_channel_id = 672827061479538714
-		self.cosmos_id = 423829836537135108
+		self.cosmos_id: int = 423829836537135108
 		self.cache = {}
 		self.report_cache = {}
+		# Teacher application attributes
+		self.app_channel_id: int = 672827061479538714
+		self.app_cat_id: int = 760114016898121758
 
 	@commands.Cog.listener()
-	async def on_ready(self):
+	async def on_ready(self) -> None:
 		print('ReportSupport cog is online!')
 
 
 	@commands.Cog.listener()
-	async def on_raw_reaction_add(self, payload):
+	async def on_raw_reaction_add(self, payload) -> None:
 		# Checks if it wasn't a bot's reaction
 		if not payload.guild_id:
 			return
 
 		if not payload.member or payload.member.bot:
 			return
+
+		guild = self.client.get_guild(payload.guild_id)
+
+		# Checks if it's in the applications channel
+		if payload.channel_id != self.app_channel_id:
+			emoji = str(payload.emoji)
+			if emoji == '✅':
+				# Gets the teacher app and does the magic
+				teacher_app = await self.get_teacher_app_by_message(payload.message_id)
+				if not teacher_app:
+					return
+
+				# Creates an interview room with the teacher and sends their application there (you can z!close there)
+				return await self.create_interview_room(guild, teacher_app)
+
+			elif emoji == '❌':
+				# Tries to delete the teacher app from the db, in case it is registered
+				return await self.delete_teacher_app(payload.message_id)
+
 
 		# Checks if the reaction was in the RepportSupport channel
 		channel = self.client.get_channel(payload.channel_id)
@@ -56,7 +79,6 @@ class ReportSupport(commands.Cog):
 		# Checks which reaction they reacted and redirects them to the respective option.
 		mid = payload.message_id
 		emoji = payload.emoji
-		guild = self.client.get_guild(payload.guild_id)
 
 		if mid == 729455417742327879 and str(emoji) == '✅':
 			# Apply to be a teacher
@@ -190,8 +212,12 @@ class ReportSupport(commands.Cog):
 			await member.send(embed=embed)
 			app_channel = await self.client.fetch_channel(self.app_channel_id)
 			cosmos = discord.utils.get(app_channel.guild.members, id=self.cosmos_id)
-			await app_channel.send(content=f"{cosmos.mention}, {member.mention}\n{app}")
+			app = await app_channel.send(content=f"{cosmos.mention}, {member.mention}\n{app}")
+			await app.add_reaction('✅')
 			self.cache[member.id] = time.time()
+			# Saves in the database
+			await self.save_teacher_app(app.id, member.id)
+						
 		else:
 			await member.send("**Let's do it again then! If you want to cancel your application, let it timeout!**")
 			return await self.send_application(member)
@@ -578,6 +604,213 @@ class ReportSupport(commands.Cog):
 					if command_index > 0:
 						command_index -= 1
 					continue
+
+
+	# Discord methods
+	async def create_interview_room(self, guild: discord.Guild, teacher_app: List[str]) -> None:
+		""" Creates an interview room with the teacher """
+
+		app_channel = self.client.get_channel(self.app_channel_id)
+		app_cat = discord.utils.get(guild.categories, id=self.app_cat_id)
+
+		msg = await app_channel.fetch_message(teacher_app[0][0])
+		teacher = discord.utils.get(guild.members, id=teacher_app[0][1])
+
+
+		#moderator = discord.utils.get(guild.roles, id=moderator_role_id)
+		cosmos = discord.utils.get(guild.members, id=self.cosmos_id)
+		admin = discord.utils.get(guild.roles, id=admin_role_id)
+
+		# Creates channels
+		overwrites: Dict = {guild.default_role: discord.PermissionOverwrite(
+			read_messages=False, send_messages=False, connect=False, view_channel=False), 
+		teacher: discord.PermissionOverwrite(
+			read_messages=True, send_messages=True, connect=False, view_channel=True), 
+		}
+		#moderator: discord.PermissionOverwrite(read_messages=True, send_messages=True, connect=False, view_channel=True, manage_messages=True)
+		txt_channel = await guild.create_text_channel(name=f"{teacher.name}'s-interview", category=app_cat, overwrites=overwrites)
+		vc_channel = await app_cat.create_voice_channel(name=f"{teacher.name}'s Interview", overwrites=overwrites)
+
+		# Updates the teacher's application in the database, adding the channels ids
+		await self.update_teacher_application(teacher.id, txt_channel.id, vc_channel.id)
+
+		app_embed = discord.Embed(
+			title=f"{teacher.name}'s Interview",
+			description=f'''
+			Hello, {teacher.mention}, we have received and reviewed your teacher application. In order to set up your lesson and explain how our system works we have to schedule a voice conversation with you. 
+			When would be the best time to talk to one our staff?''',
+			color=teacher.color)
+		await txt_channel.send(content=f"{cosmos.mention}, {admin.mention}, {teacher.mention}", embed=app_embed)
+
+
+	# In-game commands
+	@commands.command()
+	@commands.has_permissions(administrator=True)
+	async def close_app(self, ctx) -> None:
+		'''
+		(ADMIN) Closes an application channel.
+		'''
+
+		# Checks if the channel is in the teacher applications category
+		if not ctx.channel.category or not ctx.channel.category.id == self.app_cat_id:
+			return await ctx.send(f"**This is not an application channel, {ctx.author.mention}!**")
+
+
+		app_channel = await self.get_teacher_app_by_channel(ctx.channel.id)
+		if app_channel:
+			txt_channel = discord.utils.get(ctx.guild.channels, id=app_channel[0][3])
+			vc_channel = discord.utils.get(ctx.guild.channels, id=app_channel[0][4])
+			embed = discord.Embed(title="Confirmation",
+				description="Are you sure that you want to delete this application channel?",
+				color=ctx.author.color,
+				timestamp=ctx.message.created_at)
+			confirmation = await ctx.send(content=ctx.author.mention, embed=embed)
+			await confirmation.add_reaction('✅')
+			await confirmation.add_reaction('❌')
+			try:
+				reaction, user = await self.client.wait_for('reaction_add', timeout=20, 
+					check=lambda r, u: u == ctx.author and r.message.id == confirmation.id and str(r.emoji) in ['✅', '❌'])
+			except asyncio.TimeoutError:
+				embed = discord.Embed(title="Confirmation",
+				description="You took too long to answer the question; not deleting it!",
+				color=discord.Color.red(),
+				timestamp=ctx.message.created_at)
+				return await confirmation.edit(content=ctx.author.mention, embed=embed)
+			else:
+				if str(reaction.emoji) == '✅':
+					embed.description = f"**Application channel {txt_channel.mention} is being deleted...**"
+					await confirmation.edit(content=ctx.author.mention, embed=embed)
+					await asyncio.sleep(3)
+					await txt_channel.delete()
+					await vc_channel.delete()
+					await self.delete_teacher_app(app_channel[0][0])
+					try:
+						all_apps_channel = discord.utils.get(ctx.guild.channels, id=self.app_channel_id)
+						msg = await all_apps_channel.fetch_message(app_channel[0][0])
+						#await msg.delete()
+						await msg.add_reaction('🔒')
+					except Exception:
+						pass
+				else:
+					embed.description = "Not deleting it!"
+					await confirmation.edit(content='', embed=embed)
+		else:
+			await ctx.send(f"**What do you think that you are doing? You cannot delete this channel, {ctx.author.mention}!**")
+
+
+
+	# Database back-end methods
+	async def get_teacher_app_by_message(self, message_id: int) -> List[str]:
+		""" Gets a teacher application from the database by providing a message id. """
+		
+		mycursor, db = await self.the_data_base5()
+		await mycursor.execute("SELECT * FROM TeacherApplication WHERE message_id = %s", (message_id,))
+		teacher_app = await mycursor.fetchall()
+		await mycursor.close()
+		return teacher_app
+
+	async def get_teacher_app_by_channel(self, channel_id: int) -> List[str]:
+		""" Gets a teacher application from the database by providing a channel id. """
+		
+		mycursor, db = await self.the_data_base5()
+		await mycursor.execute("SELECT * FROM TeacherApplication WHERE txt_id = %s", (channel_id,))
+		teacher_app = await mycursor.fetchall()
+		await mycursor.close()
+		return teacher_app
+
+
+	async def save_teacher_app(self, message_id: int, teacher_id: int) -> None:
+		""" Saves a teacher application into the database. """
+		
+		mycursor, db = await self.the_data_base5()
+		await mycursor.execute('''
+			INSERT INTO TeacherApplication (message_id, teacher_id)
+			VALUES (%s, %s)''', (message_id, teacher_id)
+			)
+		await db.commit()
+		await mycursor.close()
+
+
+	async def update_teacher_application(self, teacher_id: int, txt_id: int, vc_id: int) -> None:
+		""" Updates the teacher's application; adding the txt and vc ids into it. """
+
+		mycursor, db = await self.the_data_base5()
+		await mycursor.execute('''UPDATE TeacherApplication SET 
+			channel_open = 'yes', txt_id = %s, vc_id = %s
+			WHERE teacher_id = %s''', (txt_id, vc_id, teacher_id)
+			)
+		await db.commit()
+		await mycursor.close()
+
+
+	async def delete_teacher_app(self, message_id: int) -> None:
+		""" Deletes a teacher application from the database. """
+
+		mycursor, db = await self.the_data_base5()
+		await mycursor.execute("DELETE FROM TeacherApplication WHERE message_id = %s", (message_id,))
+		await db.commit()
+		await mycursor.close()
+
+	# Database ADM commands
+	@commands.command(hidden=True)
+	@commands.has_permissions(administrator=True)
+	async def create_table_teacher_application(self, ctx) -> None:
+		""" (ADM) Creates the TeacherApplication table. """
+
+		mycursor, db = await self.the_data_base5()
+		if await self.table_teacher_application_exists():
+			return await ctx.send("**Table `TeacherApplication` already exists!**")
+
+		await mycursor.execute('''CREATE TABLE TeacherApplication (
+			message_id BIGINT, teacher_id BIGINT, channel_open VARCHAR(3) default 'no',
+			txt_id BIGINT default null, vc_id BIGINT default null)''')
+
+		await db.commit()
+		await mycursor.close()
+		await ctx.send("**Table `TeacherApplication` created!**")
+
+	@commands.command(hidden=True)
+	@commands.has_permissions(administrator=True)
+	async def drop_table_teacher_application(self, ctx) -> None:
+		""" (ADM) Drops the TeacherApplication table. """
+
+		mycursor, db = await self.the_data_base5()
+		if not await self.table_teacher_application_exists():
+			return await ctx.send("**Table `TeacherApplication` doesn't exist!**")
+
+		await mycursor.execute("DROP TABLE TeacherApplication")
+		await db.commit()
+		await mycursor.close()
+		await ctx.send("**Table `TeacherApplication` dropped!**")
+
+
+	@commands.command(hidden=True)
+	@commands.has_permissions(administrator=True)
+	async def reset_table_teacher_application(self, ctx) -> None:
+		""" (ADM) Resets the TeacherApplication table. """
+
+		mycursor, db = await self.the_data_base5()
+		if not await self.table_teacher_application_exists():
+			return await ctx.send("**Table `TeacherApplication` doesn't exist yet!**")
+
+		await mycursor.execute("DELETE FROM TeacherApplication")
+		await db.commit()
+		await mycursor.close()
+		await ctx.send("**Table `TeacherApplication` reset!**")
+
+	async def table_teacher_application_exists(self) -> bool:
+		""" Checks whether the TeacherApplication table exists. """
+
+		mycursor, db = await self.the_data_base5()
+		await mycursor.execute("SHOW TABLE STATUS LIKE 'TeacherApplication'")
+		exists = await mycursor.fetchall()
+		await mycursor.close()
+		if len(exists):
+			return True
+		else:
+			return False
+
+
 
 def setup(client):
 	client.add_cog(ReportSupport(client))
