a674947bbe81b2eeb66495e39e96b5eafe3c9cb8
Yago <yagomichalak@gmail.com>
2020-12-09 17:26:12 -0300

    adding a server status command

--- cogs/show.py
+++ cogs/show.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from mysqldb import the_database
 
 rules = [
     "Do not post or talk about NSFW content in text or voice chat. This server is a safe for work.",
@@ -53,6 +54,66 @@ class Show(commands.Cog):
                               colour=discord.Colour.dark_green())
         embed.set_footer(text=ctx.author.guild.name)
         await ctx.send(embed=embed)
+
+    @commands.command(aliases=['ss'])
+    @commands.has_permissions()
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def server_status(self, ctx) -> None:
+        """ Shows some server statuses. """
+
+        member = ctx.author
+
+        group_by += 's'.title()
+        mycursor, db = await the_database()
+        await mycursor.execute("""
+            SELECT 
+            STR_TO_DATE(complete_date, '%d/%m/%Y') AS Months,
+            SUM(m_joined) - SUM(m_left) AS 'Total Joins',
+            members AS 'First Member Record of the Month',
+            MAX(members) AS 'Last Member Record of the Month'
+            FROM DataBumps
+            GROUP BY MONTH(Months)
+            """)
+
+        months = await mycursor.fetchall()
+        await mycursor.close()
+
+        embed = discord.Embed(
+            title="__Server's Monthly Statuses__",
+            description="**N**: Month counting;\n"\
+            +"**Date**: The day of the month that the bot started registering server statuses;\n"\
+            +"**Joins**: The total of users who joined the server in the month;\n"\
+            +"**FtRec**: First record of total members of the month;\n"\
+            +"**LtRec**: Last record of total members of the month.",
+            color=member.color,
+            timestamp=ctx.message.created_at,
+            url="http://thelanguagesloth.com"
+        )
+
+        temp_text1 = f"{'N':>2} | {'Date':<4} | {'Joins':^5} | {'FtRec':>5} | {'LtRec':>5}"
+
+        embed.add_field(
+            name=f"{'='*35}",
+            value=f"```apache\n{temp_text1}```",
+            inline=False
+        )
+
+        temp_text2 = ''
+        # the_months = {1: '',2: '',3: '',4: '',5: '',6: '',7: '',8: '',9: '',10: '',11: '',12: ''}
+        for i, month in enumerate(months):
+            the_month = month[0].strftime('%b')
+            temp_text2 += f"{i+1:>2} | {the_month:<4} | {month[1]:^5} | {month[2]:>5} | {month[3]:>5}\n"
+
+        embed.add_field(
+            name=f"{'='*35}",
+            value=f"```apache\n{temp_text2}```",
+            inline=False
+        )
+
+        embed.set_author(name=member, icon_url=member.avatar_url)
+        embed.set_footer(text=member.guild, icon_url=member.guild.icon_url)
+        await ctx.send(embed=embed)
+
 
 
 def setup(client):
