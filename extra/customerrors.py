fd4817b7af55413469157695d21aea94ac636979
Yago <yagomichalak@gmail.com>
2021-01-10 18:16:42 -0300

    steal and divine_protection commands done

--- extra/customerrors.py
+++ extra/customerrors.py
@@ -8,3 +8,9 @@ class MissingRequiredSlothClass(commands.CheckFailure):
 	def __init__(self, required_class: str, error_message: str) -> None:
 		self.required_class = required_class
 		self.error_message = error_message
+
+class ActionSkillOnCooldown(commands.CheckFailure): 
+
+	def __init__(self, try_after: int, error_message: str) -> None:
+		self.try_after = try_after
+		self.error_message = error_message
