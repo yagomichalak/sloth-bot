73699b2994923e65f6153d34468d0dd58a19316b
Yago Michalak <56281335+yagomichalak@users.noreply.github.com>
2021-06-07 21:34:03 -0300

    DELETED some imports and UPDATED some texts
    Either the datetime and aiohttp libraries were not being used at all, so I don't see why you need those in the code.

--- cogs/programmingchannels.py
+++ cogs/programmingchannels.py
@@ -1,24 +1,19 @@
 import discord
 from discord.ext import commands
-from datetime import datetime
-import aiohttp
-
 
 class ProgrammingChannels(commands.Cog):
-    '''
-    Collection of programming channel related commands.
-    '''
+    """ Collection of programming channel related commands. """
 
     def __init__(self, client):
         self.client = client
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print("Misc cog is online!")
+        print("ProgrammingChannels cog is online!")
 
     @commands.command()
     async def justask(self, ctx):
-        '''Posts link to "Don't ask to ask, just ask"'''
+        """ Posts link to "Don't ask to ask, just ask" """
         await ctx.message.delete()
         em = discord.Embed(color=ctx.author.color, title=f"Don't ask to ask, just ask.",
                            timestamp=ctx.message.created_at, url='https://dontasktoask.com/')
