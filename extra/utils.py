3fa6fa420b5e30e8e62f3b3ca5c7c4bfee982d52
unknown <yagomichalak@gmail.com>
2021-06-18 10:44:53 -0300

    fixing timezone of curernt timestamp etc/gmt

--- extra/utils.py
+++ extra/utils.py
@@ -1,12 +1,16 @@
 from datetime import datetime
+from mysqldb import the_database
 import re
+from pytz import timezone
 
 async def get_timestamp() -> int:
     """ Gets the current timestamp. """
 
     epoch = datetime.utcfromtimestamp(0)
-    the_time = (datetime.utcnow() - epoch).total_seconds()
-    return the_time
+    # - epoch#).total_seconds()
+    tzone = timezone('Etc/GMT')
+    the_time = datetime.now(tzone)
+    return the_time.timestamp()
 
 async def parse_time() -> str:
     return datetime(*map(int, re.split(r'[^\d]', str(datetime.utcnow()).replace('+00:00', ''))))
