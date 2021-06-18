586e70b2ce90d7e57ab99f86de17f19ab411ed07
unknown <yagomichalak@gmail.com>
2021-06-18 10:53:58 -0300

    changing timezone of a command

--- extra/utils.py
+++ extra/utils.py
@@ -6,8 +6,6 @@ from pytz import timezone
 async def get_timestamp() -> int:
     """ Gets the current timestamp. """
 
-    epoch = datetime.utcfromtimestamp(0)
-    # - epoch#).total_seconds()
     tzone = timezone('Etc/GMT')
     the_time = datetime.now(tzone)
     return the_time.timestamp()
