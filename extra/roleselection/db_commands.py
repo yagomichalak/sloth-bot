b17b7ecc6dcf54cceefe29d71eccfdd4e67238b7
= <=>
2021-07-20 14:59:40 -0300

    FINISHED the RoleSelection menu system

---
+++ extra/roleselection/db_commands.py
@@ -0,0 +1,193 @@
+import discord
+from discord import guild
+from discord.ext import commands
+from mysqldb import the_database
+from typing import List, Union
+
+
+class RoleSelectionDatabaseCommands(commands.Cog):
+
+    def __init__(self, client) -> None:
+        self.client = client
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def create_table_selection_menu(self, ctx: commands.Context) -> None:
+        """ (OWNER) Creates the SelectionMenu table. """
+
+        await ctx.message.delete()
+        member = ctx.author
+
+        if await self.check_table_selection_menu_exists():
+            return await ctx.send(f"**Table `SelectionMenu` already exists, {member.mention}!**", delete_after=5)
+
+        mycursor, db = await the_database()
+        await mycursor.execute("""
+        CREATE TABLE SelectionMenu (
+            message_id BIGINT NOT NULL,
+            channel_id BIGINT NOT NULL,
+            guild_id BIGINT NOT NULL,
+
+            style TINYINT(1) NOT NULL,
+            label VARCHAR(80) NOT NULL,
+            emoji VARCHAR(150) NOT NULL,
+            custom_id VARCHAR(100) NOT NULL
+        )
+        """)
+        await db.commit()
+        await mycursor.close()
+        await ctx.send("**Table `SelectionMenu` has been created!**", delete_after=5)
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def drop_table_selection_menu(self, ctx: commands.Context) -> None:
+        """ (OWNER) Drops the SelectionMenu table. """
+
+        await ctx.message.delete()
+        member = ctx.author
+
+        if not await self.check_table_selection_menu_exists():
+            return await ctx.send(f"**Table `SelectionMenu` doesn't exist, {member.mention}!**", delete_after=5)
+
+        mycursor, db = await the_database()
+        await mycursor.execute("DROP TABLE SelectionMenu")
+        await db.commit()
+        await mycursor.close()
+        await ctx.send("**Table `SelectionMenu` has been dropped!**", delete_after=5)
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def reset_table_selection_menu(self, ctx: commands.Context) -> None:
+        """ (OWNER) Resets the SelectionMenu table. """
+
+        await ctx.message.delete()
+        member = ctx.author
+
+        if not await self.check_table_selection_menu_exists():
+            return await ctx.send(f"**Table `SelectionMenu` doesn't exist yet, {member.mention}!**", delete_after=5)
+
+        mycursor, db = await the_database()
+        await mycursor.execute("DELETE FROM SelectionMenu")
+        await db.commit()
+        await mycursor.close()
+        await ctx.send("**Table `SelectionMenu` has been reset!**", delete_after=5)
+
+    async def check_table_selection_menu_exists(self) -> bool:
+        """ Checks whether the SelectionMenu table exists. """
+
+        mycursor, _ = await the_database()
+        await mycursor.execute("SHOW TABLE STATUS LIKE 'SelectionMenu'")
+        exists = await mycursor.fetchone()
+        await mycursor.close()
+        if exists:
+            return True
+        else:
+            return False
+
+    # ===== Database Methods =====
+    @staticmethod
+    async def get_button_by_id(custom_id: str, message_id: int, guild_id: int) -> List[Union[int, str]]:
+        """ Gets a button from its custom ID.
+        :param custom_id: The custom ID of the button.
+        :param message_id: The message ID in which the button is in.
+        :param guild_id: The ID of the guild in which the menu is in. """
+
+        mycursor, _ = await the_database()
+        await mycursor.execute("""
+        SELECT * FROM SelectionMenu WHERE custom_id = %s AND message_id = %s AND guild_id = %s
+        """, (custom_id, message_id, guild_id))
+        button = await mycursor.fetchone()
+        await mycursor.close()
+        return button
+        
+
+    @staticmethod
+    async def get_selection_menus() -> List[List[int]]:
+        """ Gets all registered Selection Menus. """
+
+        mycursor, _ = await the_database()
+        await mycursor.execute("SELECT * FROM SelectionMenu ORDER BY guild_id, channel_id, message_id")
+        selection_menus = await mycursor.fetchall()
+        await mycursor.close()
+        return selection_menus
+
+    @staticmethod
+    async def get_selection_menu(message_id: int, guild_id: int) -> List[int]:
+        """ Gets a specific Selection Menu.
+        :param message_id: The message ID of the Selection Menu.
+        :param guild_id: The server ID in which the Selection Menu was created. """
+
+        mycursor, _ = await the_database()
+        await mycursor.execute("SELECT * FROM SelectionMenu WHERE message_id = %s AND guild_id = %s", (message_id, guild_id))
+        selection_menu = await mycursor.fetchone()
+        await mycursor.close()
+        return selection_menu
+
+    @staticmethod
+    async def insert_menu_button(
+        message_id: int, channel_id: int, guild_id: int, style: int, label: str, emoji: str, custom_id: str) -> None:
+        """ Inserts a Selection Menu.
+        :param message_id: The ID of the message in which the menu is located.
+        :param channel_id: The ID of the channel in which the menu message is located.
+        :param guild_id: The ID of the guild in which the menu was created.
+        :param style: The style of the button. (1-4)
+        :param label: The label of the button.
+        :param emoji: The emoji of the button.
+        :param custom_id: The custom ID of the button. """
+
+        mycursor, db = await the_database()
+        await mycursor.execute("""INSERT INTO SelectionMenu (
+            message_id, channel_id, guild_id, style, label, emoji, custom_id) VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """, (message_id, channel_id, guild_id, style, label, emoji, custom_id))
+        await db.commit()
+        await mycursor.close()
+
+
+
+    # ===== Delete methods =====
+
+    @staticmethod
+    async def delete_menu_button(message_id: int, channel_id: int, guild_id: int, custom_id: str) -> None:
+        """ Deletes a button from a Role Selection menu.
+        :param message_id:
+        :param channel_id:
+        :param guild_id:
+        :param custom_id: """
+
+        mycursor, db = await the_database()
+        await mycursor.execute("""
+        DELETE FROM SelectionMenu
+        WHERE message_id = %s AND channel_id = %s AND guild_id = %s AND custom_id = %s
+        """, (message_id, channel_id, guild_id, custom_id))
+
+    @staticmethod
+    async def delete_selection_menu_by_message_id(message_id: int, guild_id: int) -> None:
+        """ Deletes a Role Selection menu by message ID.
+        :param message_id: The message ID of the menu.
+        :param guild_id: The ID of the server in which the Selection Menu was created. """
+
+        mycursor, db = await the_database()
+        await mycursor.execute("DELETE FROM SelectionMenu WHERE message_id = %s AND guild_id = %s", (message_id, guild_id))
+        await db.commit()
+        await mycursor.close()
+
+    @staticmethod
+    async def delete_selection_menu_by_channel_id(channel_id: int, guild_id: int) -> None:
+        """ Deletes a Role Selection menu by channel ID.
+        :param channel_id: The ID of the channel in which the Selection Menu was created.
+        :param guild_id: The ID of the server in which the Selection Menu was created. """
+
+        mycursor, db = await the_database()
+        await mycursor.execute("DELETE FROM SelectionMenu WHERE channel_id = %s AND guild_id = %s", (channel_id, guild_id))
+        await db.commit()
+        await mycursor.close()
+
+    @staticmethod
+    async def delete_selection_menu_by_guild_id(guild_id: int) -> None:
+        """ Deletes a Role Selection menu by guild ID.
+        :param guild_id: The ID of the server in which the Selection Menu was created. """
+
+        mycursor, db = await the_database()
+        await mycursor.execute("DELETE FROM SelectionMenu WHERE guild_id = %s", (guild_id,))
+        await db.commit()
+        await mycursor.close()
---
+++ extra/roleselection/menu.py
@@ -0,0 +1,185 @@
+import discord
+from discord.ext import commands
+
+from extra.prompt.menu import ConfirmButton, prompt_number, Confirm
+from extra.roleselection.db_commands import RoleSelectionDatabaseCommands
+from extra.roleselection.utils import callback as button_callback
+
+from functools import partial
+
+
+
+class ManageRoleSelectionMenu(discord.ui.View):
+	
+	def __init__(self, client, message: discord.Message) -> None:
+		super().__init__(timeout=60)
+		self.client = client
+		self.message = message
+		self.db = RoleSelectionDatabaseCommands
+
+	@discord.ui.button(style=discord.ButtonStyle.success, label="Add Button", custom_id="add_selection_menu_button")
+	async def add_button(self, button: discord.ui.Button, interaction: discord.Interaction) -> None:
+
+		await interaction.response.defer()
+		member = interaction.user
+		view_menu = discord.ui.View.from_message(self.message, timeout=None)
+
+		ctx = await self.client.get_context(interaction.message)
+		ctx.author = member
+		cog = self.client.get_cog('RoleSelection')
+		# print('\n'.join([f"button: {btn.label} custom id {btn.custom_id}" for btn in view.children]))
+
+
+
+		self.stop()
+		while True:
+
+			button_configs = await cog.ask_button_questions(ctx, self.message)
+			if not button_configs:
+				break
+
+			view_menu.add_item(RoleButton(**button_configs))
+			for child in view_menu.children:
+				child.callback = partial(button_callback, child)
+
+			try:
+				await self.db.insert_menu_button(self.message.id, self.message.channel.id, self.message.guild.id, *button_configs.values())
+			except Exception as e:
+				print(e)
+				await interaction.channel.send(f"**I couldn't add your button to the database, {member.mention}!**", delete_after=5)
+			else:
+				await self.message.edit(view=view_menu)
+
+			view = ConfirmButton()
+			embed = discord.Embed(description=f"**Wanna add more buttons into your menu, {member.mention}?**")
+			msg = await interaction.channel.send(embed=embed, view=view)
+			
+			
+			await view.wait()
+			await msg.delete()
+			if view.value is None or not view.value:
+				break
+			
+		await self.message.channel.send("**Menu updated!**", delete_after=5)
+
+	@discord.ui.button(style=discord.ButtonStyle.danger, label="Delete Button", custom_id="delete_selection_menu_button")
+	async def delete_button(self, button: discord.ui.Button, interaction: discord.Interaction) -> None:
+
+		await interaction.response.defer()
+		self.stop()
+
+		member = interaction.user
+		view_menu = discord.ui.View.from_message(self.message, timeout=60)		
+
+		buttons_text = [f"`{i+1}`: ({child.emoji}) {child.label} <@&{child.custom_id}>" for i, child in enumerate(view_menu.children)]
+
+		if not buttons_text:
+			return await interaction.channel.send(f"**You have no buttons in this menu yet, {member.mention}!**", delete_after=3)
+
+		embed = discord.Embed(
+			title="__Available Buttons in this Menu__",
+			description='\n'.join(buttons_text),
+			color=member.color,
+			timestamp=self.message.created_at
+		)
+		embed.set_footer(text=f"Menu: {self.message.id}")
+		
+		await interaction.channel.send(f"**What button you want to delete, {member.mention}?**", embed=embed)
+		number = await prompt_number(self.client, interaction.channel, member, limit=len(view_menu.children))
+		if not number:
+			return
+
+		view = ConfirmButton()
+		chosen_button = view_menu.children[number-1]
+		embed = discord.Embed(
+			description=f"""**Are you sure you want to delete the button with label `{chosen_button.label}`,
+			with the <@&{chosen_button.custom_id}> role attached to it, {member.mention}?**"""
+		)
+
+		msg = await interaction.channel.send(embed=embed, view=view)
+		# Wait for the View to stop listening for input...
+		await view.wait()
+		if view.value is None:
+			await interaction.channel.send(f"**Timeout, {member.mention}!**", delete_after=5)
+		elif view.value:
+			view_menu.children = [
+				child for child in view_menu.children 
+				if child != chosen_button
+			]
+
+			for child in view_menu.children:
+				child.callback = partial(button_callback, child)
+			
+			try:
+				await self.db.delete_menu_button(
+					self.message.id, self.message.channel.id, self.message.guild.id, chosen_button.custom_id)
+			except Exception as e:
+				await interaction.channel.send(f"**I couldn't remove your button from the database, {member.mention}!**", delete_after=5)
+			else:
+				await self.message.edit(view=view_menu)
+			
+			# await self.client.add_view(view=view_menu, message_id=self.message.id)
+			await interaction.channel.send(f"**Menu successfully deleted, {member.mention}!**", delete_after=5)
+		else:
+			await interaction.channel.send(f"**Not deleting it then, {member.mention}!**", delete_after=5)
+
+		await msg.delete()
+
+	@discord.ui.button(style=discord.ButtonStyle.danger, label="Delete Menu", custom_id="delete_whole_selection_menu")
+	async def delete_menu(self, button: discord.ui.Button, interaction: discord.Interaction) -> None:
+		
+		await interaction.response.defer()
+		member = interaction.user
+		view = ConfirmButton()
+		self.stop()
+		msg = await interaction.channel.send(
+			f"**Are you sure you want to delete this menu, {member.mention}?**", view=view)
+		# Wait for the View to stop listening for input...
+		await view.wait()
+		if view.value is None:
+			await interaction.channel.send(f"**Timeout, {member.mention}!**", delete_after=5)
+		elif view.value:
+			await self.db.delete_selection_menu_by_message_id(self.message.id, self.message.guild.id)
+			await self.message.delete()
+			await interaction.channel.send(f"**Menu successfully deleted, {member.mention}!**", delete_after=5)
+		else:
+			await interaction.channel.send(f"**Not deleting it then, {member.mention}!**", delete_after=5)
+
+		try:
+			await msg.delete()
+		except:
+			pass
+		
+
+	@discord.ui.button(style=discord.ButtonStyle.secondary, label="Cancel", custom_id="cancel_selection_menu")
+	async def cancel_menu(self, button: discord.ui.Button, interaction: discord.Interaction) -> None:
+		self.stop()
+
+
+		
+
+
+# ===== Button-based =====
+class RoleButton(discord.ui.Button):
+
+	# def __init__(self, **kwargs) -> None:
+	# 	super().__init__(kwargs)
+
+
+	async def callback(self, interaction: discord.Interaction) -> None:
+
+		member = interaction.user
+
+		try:
+			role = discord.utils.get(member.guild.roles, id=int(self.custom_id))
+		except Exception as e:
+			print(e)
+			pass
+		else:
+			member_roles_ids = [r.id for r in member.roles]
+			if role and role.id not in member_roles_ids:
+				await member.add_roles(role)
+				await interaction.response.send_message(f"**The `{role}` role was assigned to you!**", ephemeral=True)
+			else:
+				await member.remove_roles(role)
+				await interaction.response.send_message(f"**The `{role}` role was taken away from you!**", ephemeral=True)
