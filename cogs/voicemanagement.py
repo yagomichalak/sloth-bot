c8ae4d4197cfeb55c17cd0dddfff3ce7536b9e33
= <=>
2021-08-11 18:39:34 -0300

    Made a system for disconnecting ppl from the Video Voice calls channel when they don't have cameras on

---
+++ cogs/voicemanagement.py
@@ -0,0 +1,100 @@
+import discord
+from discord.ext import commands, tasks
+import os
+from typing import Dict, Union
+from extra import utils
+
+server_id = int(os.getenv('SERVER_ID'))
+
+class VoiceManagement(commands.Cog):
+
+    def __init__(self, client) -> None:
+        self.client = client
+        self.vcc_id: int = int(os.getenv('VOICE_CALLS_CHANNEL_ID'))
+
+        # user_id: {'timestamp': 123, 'camera_on': False, 'notified': False}
+
+        self.people: Dict[int, Dict[str, Union[int, bool]]] = {}
+
+
+
+    @commands.Cog.listener()
+    async def on_ready(self) -> None:
+        self.check_camera_on.start()
+        print('VoiceManagement cog is online!')
+
+    @tasks.loop(seconds=60)
+    async def check_camera_on(self) -> None:
+        """ Checks whether people in the Video Calls channel have their cameras on. """
+
+        current_ts = await utils.get_timestamp()
+        guild = self.client.get_guild(server_id)
+
+        for user_id in list(self.people.keys()):
+            secs = current_ts - self.people[user_id]['timestamp']
+            if secs >= 60 and secs < 180:
+                if not self.people[user_id]['camera_on'] and not self.people[user_id]['notified']:
+                    # Notifies user to turn on camera
+                    try:
+                        member = guild.get_member(user_id)
+                        await member.send(
+                            f"**Hey, I saw you are in the `Video Calls` channel and didn't turn on your camera. Please, do it or you will soon get disconnected!**")
+                        self.people[user_id]['notified'] = True
+                    except:
+                        pass
+
+            elif secs >= 180:
+                if not self.people[user_id]['camera_on']:
+                    del self.people[user_id]
+                    # Disconnects users with cameras off
+                    try:
+                        member = guild.get_member(user_id)
+                        await member.move_to(None)
+                        await member.send(
+                            f"**You got disconnected for not turning on your camera in the `Video Calls` voice channel!**")
+                    except:
+                        pass
+
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+        """ Checks whether people have open cameras in the voice channel. """
+
+        if member.bot:
+            return
+
+        # Check voice states
+        if before.mute != after.mute:
+            return
+        if before.deaf != before.deaf:
+            return
+        if before.self_mute != after.self_mute:
+            return
+        if before.self_deaf != after.self_deaf:
+            return
+        if before.self_stream != after.self_stream:
+            return
+
+        # Get before/after channels and their categories
+        bc = before.channel
+        ac = after.channel
+
+        current_ts = await utils.get_timestamp()
+
+        # Joining the Video Calls channel
+        if ac and ac.id == self.vcc_id:
+            self.people[member.id] = {
+                'timestamp': current_ts,
+                'camera_on': after.self_video,
+                'notified': False
+            }
+
+        # Leaving the Video Calls channel
+        elif not ac or ac.id != self.vcc_id:
+            self.people.pop(member.id, None)
+
+
+
+
+def setup(client) -> None:
+    client.add_cog(VoiceManagement(client))
