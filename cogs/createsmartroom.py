640aeb5e8126c7bb3f560aab8cf17284c5501a68
Prisca <priscasevgi@gmail.com>
2021-09-18 14:51:30 +0300

    galaxy room last line change

--- cogs/createsmartroom.py
+++ cogs/createsmartroom.py
@@ -1023,8 +1023,8 @@ Here are some rules and rights:
 
 3) You have to allow a minimum of **10** users. (Bots or admins do not count) 
 
-4) You can add up to 4 **threads** each for **250 :leaves: **by writing **z!agc text (name of the channel) ** **or** for **500** :leaves:  1 additional voice channel by writing **z!agc voice (name of the channel) ** 
-You can only add either **threads ** **OR** one **voice channel **
+4) You can add up to 4 **threads** each for **250 :leaves: **by writing **z!agc text (name of the channel) ** **or** for **500** :leaves:  1 additional voice channel by writing **z!agc voice (name of the channel)** 
+You can only add either **threads** **OR** one **voice channel**
 
 If you are planning on using one room as a botroom/music room you should use your main channel for that since most music bots don't respond in threads yet"""))
 
