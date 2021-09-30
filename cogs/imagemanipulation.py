7bbe548169d8f52c9f727f1438f50904060193ca
yagomichalak <yagomichalak@gmail.com>
2021-09-30 11:39:25 -0300

    MADE commands for lightening and darkening images, along with other placeholders for upcoming image-related commands

--- cogs/imagemanipulation.py
+++ cogs/imagemanipulation.py
@@ -5,7 +5,7 @@ from extra import utils
 from PIL import Image, ImageDraw, ImageFont
 from typing import Union, List, Any, Optional
 import os
-
+from io import BytesIO
 
 class ImageManipulation(commands.Cog):
     """ Categories for image manipulations and visualization. """
@@ -116,6 +116,115 @@ class ImageManipulation(commands.Cog):
         :param member: The member to implode the profile picture. [Optional][Default = Cached Image] """
 
         pass
+
+    @commands.command()
+    @utils.not_ready()
+    async def rotates(self, ctx, member: Optional[discord.Member] = None) -> None:
+        """ Rotates an image.
+        :param member: The member to rotate the profile picture from. [Optional][Default = Cached Image] """
+
+        pass
+
+    @commands.command(aliases=['light', 'brighten', 'bright'])
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def lighten(self, ctx, member: Optional[discord.Member] = None, percentage: int = 55) -> None:
+        """ Lightens an image.
+        :param member: The member of whom to lighten the picture. [Optional][Default = Cached Image] """
+
+        file: Any = None
+
+        if member:
+            file = member.display_avatar
+        else:
+            if self.cached_image:
+                file = self.cached_image
+            else:
+                file = ctx.author.display_avatar
+        image: Image.Image = await self.change_image_brightness(file=file, action='lighten', percentage=percentage)
+        self.cached_image = image
+        embed = discord.Embed(
+            color=int('36393F', 16)
+        )
+
+        embed.set_image(url='attachment://lightened_image.png')
+        bytes_image = await self.image_to_byte_array(image)
+        await ctx.reply(embed=embed, file=discord.File(BytesIO(bytes_image), 'lightened_image.png'))
+
+    @commands.command(aliases=['dark', 'dim'])
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def darken(self, ctx, member: Optional[discord.Member] = None, percentage: int = 55) -> None:
+        """ Darkens an image.
+        :param member: The member of whom to darken the profile picture. [Optional][Default = Cached Image] """
+
+        file: Any = None
+
+        if member:
+            file = member.display_avatar
+        else:
+            if self.cached_image:
+                file = self.cached_image
+            else:
+                file = ctx.author.display_avatar
+        image: Image.Image = await self.change_image_brightness(file=file, action='darken', percentage=percentage)
+        self.cached_image = image
+        embed = discord.Embed(
+            color=int('36393F', 16)
+        )
+
+        embed.set_image(url='attachment://darkened_image.png')
+        bytes_image = await self.image_to_byte_array(image)
+        await ctx.reply(embed=embed, file=discord.File(BytesIO(bytes_image), 'darkened_image.png'))
+
+    async def image_to_byte_array(self, image: Image.Image) -> bytes:
+        """ Converts an image to bytes.
+        :param image: The image to convert to bytes. """
+
+        imgByteArr = BytesIO()
+        image.save(imgByteArr, format='png')
+        imgByteArr = imgByteArr.getvalue()
+        return imgByteArr
+
+    async def change_image_brightness(self, file: Any, action: str, percentage: int) -> None:
+        """ Changes the brightness level of each pixel of the image.
+        :param file: The file to execute the task on.
+        :param action: Whether to lighten or darken the image.
+        :param percentage: The percentage to ligthen or darken the image. """
+
+        if not isinstance(file, Image.Image):
+            original_image = Image.open(BytesIO(await file.read()))
+        else:
+            original_image = file
+        pixels = original_image.getdata()
+
+        #initialise the new image
+        new_image = Image.new('RGB', original_image.size)
+        new_image_list = []
+
+        brightness_multiplier = 1.0
+
+        if action == 'lighten':
+            brightness_multiplier += (percentage/100)
+        else:
+            brightness_multiplier -= (percentage/100)
+
+        #for each pixel, append the brightened or darkened version to the new image list
+        for pixel in pixels:
+            new_pixel = (int(pixel[0] * brightness_multiplier),
+                        int(pixel[1] * brightness_multiplier),
+                        int(pixel[2] * brightness_multiplier))
+
+            #check the new pixel values are within rgb range
+            for pixel in new_pixel:
+                if pixel > 255:
+                    pixel = 255
+                elif pixel < 0:
+                    pixel = 0
+
+            new_image_list.append(new_pixel)
+
+        #save the new image
+        new_image.putdata(new_image_list)
+        return new_image
 
 
 def setup(client: commands.Cog) -> None:
