9ffd9fa50b8691b53e98728a5b973bf08b26cb2f
yagomichalak <yagomichalak@gmail.com>
2021-11-07 14:18:19 -0300

    MADE Munk's 4th skill; get_quest

--- cogs/slothclass.py
+++ cogs/slothclass.py
@@ -280,7 +280,7 @@ class SlothClass(*classes.values(), db_commands.SlothClassDatabaseCommands):
         embed.add_field(name="🟢 Cybersloth's 4th Skill:", value="**Skill**: `Lock`.", inline=True)
         embed.add_field(name="🔴 Merchant's 4th Skill:", value="**Skill**: `Sell Pet`.", inline=False)
         embed.add_field(name="🔴 Metamorph's 4th Skill:", value="**Skill**: `Reborn`.", inline=True)
-        embed.add_field(name="🔴 Munk's 4th Skill:", value="**Skill**: `Get Quest`.", inline=True)
+        embed.add_field(name="🟠 Munk's 4th Skill:", value="**Skill**: `Get Quest`.", inline=True)
         embed.add_field(name="🔴 Prawler's 4th Skill:", value="**Skill**: `Kidnap`.", inline=False)
         embed.add_field(name="🔴 Seraph's 4th Skill:", value="**Skill**: `Attain Grace`.", inline=True)
         embed.add_field(name="🟢 Warrior's 4th Skill:", value="**Skill**: `Poison`.", inline=True)
---
+++ extra/slothclasses/enums.py
@@ -0,0 +1,12 @@
+import discord
+import enum
+
+
+class QuestEnum(enum.Enum):
+
+    1 = 'test1'
+    2 = 'test2'
+    3 = 'test3'
+    4 = 'test4'
+    5 = 'test5'
+    6 = 'test6'
