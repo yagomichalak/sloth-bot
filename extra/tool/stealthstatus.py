3814041da2cb58e3046462f586b3b0bfb0730c58
yagomichalak <yagomichalak@gmail.com>
2021-11-25 11:15:18 -0300

    MADE a Stealth system for senior mods

---
+++ extra/tool/stealthstatus.py
@@ -0,0 +1,111 @@
+import discord
+from discord.ext import commands
+from mysqldb import the_database
+from typing import List
+
+class StealthStatusTable(commands.Cog):
+    """ Class for the StealthStatus table and its commands and methods. """
+
+    def __init__(self, client: commands.Bot) -> None:
+        """ Class init method. """
+
+        self.client = client
+
+    @commands.command(hidden=True)
+    @commands.has_permissions(administrator=True)
+    async def create_table_stealth_status(self, ctx) -> None:
+        """ (ADM) Creates the StealthStatus table. """
+
+        if await self.check_table_stealth_status_exists():
+            return await ctx.send("**Table __StealthStatus__ already exists!**")
+
+        await ctx.message.delete()
+        mycursor, db = await the_database()
+        await mycursor.execute("""
+            CREATE TABLE StealthStatus (
+                user_id BIGINT NOT NULL,
+                state TINYINT(1) DEFAULT 0,
+                PRIMARY KEY (user_id)
+            )
+        """)
+        await db.commit()
+        await mycursor.close()
+
+        return await ctx.send("**Table __StealthStatus__ created!**")
+
+    @commands.command(hidden=True)
+    @commands.has_permissions(administrator=True)
+    async def drop_table_stealth_status(self, ctx) -> None:
+        """ (ADM) Creates the StealthStatus table """
+
+        if not await self.check_table_stealth_status_exists():
+            return await ctx.send("**Table __StealthStatus__ doesn't exist!**")
+        await ctx.message.delete()
+        mycursor, db = await the_database()
+        await mycursor.execute("DROP TABLE StealthStatus")
+        await db.commit()
+        await mycursor.close()
+
+        return await ctx.send("**Table __StealthStatus__ dropped!**")
+
+    @commands.command(hidden=True)
+    @commands.has_permissions(administrator=True)
+    async def reset_table_stealth_status(self, ctx):
+        """ (ADM) Resets the StealthStatus table. """
+
+        if not await self.check_table_stealth_status_exists():
+            return await ctx.send("**Table __StealthStatus__ doesn't exist yet**")
+
+        await ctx.message.delete()
+        mycursor, db = await the_database()
+        await mycursor.execute("DELETE FROM StealthStatus")
+        await db.commit()
+        await mycursor.close()
+
+        return await ctx.send("**Table __StealthStatus__ reset!**")
+
+    async def check_table_stealth_status_exists(self) -> bool:
+        """ Checks whether the StealthStatus table exists """
+
+        mycursor, _ = await the_database()
+        await mycursor.execute("SHOW TABLE STATUS LIKE 'StealthStatus'")
+        exists = await mycursor.fetchone()
+        await mycursor.close()
+
+        if exists:
+            return True
+        else:
+            return False
+
+    async def insert_stealth_status(self, user_id: int, state: int = 1) -> None:
+        """ Inserts a stealth status for a user.
+        :param user_id: The ID of the user to insert.
+        :param state: The initial stealth status to insert. [DEFAULT = 1] """
+
+        mycursor, db = await the_database()
+        await mycursor.execute("INSERT INTO StealthStatus (user_id, state) VALUES (%s, %s)", (user_id, state))
+        await db.commit()
+        await mycursor.close()
+
+    async def get_stealth_status(self, user_id: int) -> List[int]:
+        """ Gets the user's current StealthStatus state.
+        :parma user_id: The ID of the user from whom to get the status. """
+
+        mycursor, _ = await the_database()
+        await mycursor.execute("SELECT * from StealthStatus where user_id = %s", (user_id,))
+        user_status = await mycursor.fetchone()
+        await mycursor.close()
+        return user_status
+
+    async def update_stealth_status(self, user_id: int, state: int) -> None:
+        """ Updates the user's StealthStatus state.
+        :param user_id: The ID of the user to update.
+        :param state: The state of the StealthStatus to set. """
+
+        mycursor, db = await the_database()
+        await mycursor.execute("UPDATE StealthStatus SET state = %s WHERE user_id = %s", (state, user_id))
+        await db.commit()
+        await mycursor.close()
+
+
+
