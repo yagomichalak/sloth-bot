7730d97663e8af896105fef188fac5bc1cf872f6
Gabriel <fernandesg.s@hotmail.com>
2021-12-07 00:24:08 -0300

    CHANGED the co to surrender

--- extra/minigames/blackjack/blackjack_game.py
+++ extra/minigames/blackjack/blackjack_game.py
@@ -204,7 +204,7 @@ class BlackJackGame:
         self.client.loop.create_task(SlothCurrency.update_user_money(self.player_id, int(self.bet * 2.5)))
 
         # Change title and end the game
-        self.title = f"Blackjack - **{self.player_name}** won {int(self.bet * 1.5)} leaves 🍃"
+        self.title = f"Player Blackjack - **{self.player_name}** won {int(self.bet * 1.5)} leaves 🍃"
         self.status = 'finished'
         self.color = discord.Color.green()
         self.dealer_final_show()
@@ -251,7 +251,7 @@ class BlackJackGame:
     def surrender_event(self):
         # Change title and end the game
         
-        self.title = f"Surrender - **{self.player_name}** lost {int(self.bet * 0.40)} leaves 🍃"
+        self.title = f"Surrender - **{self.player_name}** lost {int(self.bet * 0.35)} leaves 🍃"
         self.color = int("ffffff", 16)
         self.status = 'finished'
         self.dealer_final_show()
