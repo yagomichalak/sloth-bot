49f1afa992a3dc2efd37c02b1c11f2a7867b4dd4
yagomichalak <yagomichalak@gmail.com>
2021-12-17 13:02:33 -0300

    MADE Seraph's 4th skill; attain_grace

--- extra/slothclasses/userbabies.py
+++ extra/slothclasses/userbabies.py
@@ -23,7 +23,8 @@ class UserBabiesTable(commands.Cog):
 
         mycursor, db = await the_database()
         await mycursor.execute("""CREATE TABLE UserBabies (
-            user_id BIGINT NOT NULL,
+            parent_one BIGINT NOT NULL,
+            parent_two BIGINT NOT NULL,
             baby_name VARCHAR(25) DEFAULT 'Embryo',
             baby_class VARCHAR(25) DEFAULT 'Embryo',
             PRIMARY KEY (user_id)
@@ -78,56 +79,59 @@ class UserBabiesTable(commands.Cog):
         else:
             return False
 
-    async def insert_user_baby(self, user_id: int, baby_name: Optional[str] = None, baby_class: Optional[str] = None) -> None:
+    async def insert_user_baby(self, parent_one: int, parent_two: int, baby_name: Optional[str] = None, baby_class: Optional[str] = None) -> None:
         """ Inserts a User Baby.
-        :param user_id: The ID of the user owner of the baby.
+        :param parent_one: The parent one of the baby.
+        :param parent_two: The parent two of the baby.
         :param baby_name: The name of the baby. [Optional]
         :param baby_class: The class of the baby. [Optional]"""
 
         mycursor, db = await the_database()
         if baby_name and baby_class:
-            await mycursor.execute("INSERT INTO UserBabies (user_id, baby_name, baby_class) VALUES (%s, %s, %s)", (user_id, baby_name, baby_class))
+            await mycursor.execute("""
+                INSERT INTO UserBabies (parent_one, parent_two, baby_name, baby_class) VALUES (%s, %s, %s, %s)
+                """, (parent_one, parent_two, baby_name, baby_class))
         else:
-            await mycursor.execute("INSERT INTO UserBabies (user_id) VALUES (%s)", (user_id,))
+            await mycursor.execute("INSERT INTO UserBabies (user_id, parent_one, parent_two) VALUES (%s, %s)", (parent_one, parent_two))
         await db.commit()
         await mycursor.close()
 
-    async def get_user_baby(self, user_id: int) -> List[Union[str, int]]:
+    async def get_user_baby(self, parent_id: int) -> List[Union[str, int]]:
         """ Get the user's baby.
-        :param user_id: The ID of the baby's owner. """
+        :param parent_id: The ID of one of the baby's parent. """
 
         mycursor, _ = await the_database()
-        await mycursor.execute("SELECT * FROM UserBabies WHERE user_id = %s", (user_id,))
+        await mycursor.execute("SELECT * FROM UserBabies WHERE parent_one = %s OR parent_two = %s", (parent_id, parent_id))
         user_baby = await mycursor.fetchone()
         await mycursor.close()
         return user_baby
 
-    async def update_user_baby_name(self, user_id: int, baby_name: str) -> None:
+    async def update_user_baby_name(self, parent_id: int, baby_name: str) -> None:
         """ Updates the User Baby's name.
-        :param user_id: The ID of the baby's owner.
+        :param parent_id: The ID of one of the baby's parent.
         :param baby_name: The new baby name to update to. """
 
         mycursor, db = await the_database()
-        await mycursor.execute("UPDATE UserBabies SET baby_name = %s WHERE user_id = %s", (baby_name, user_id))
+        await mycursor.execute("UPDATE UserBabies SET baby_name = %s WHERE parent_one = %s OR parent_two = %s", (baby_name, parent_id, parent_id))
         await db.commit()
         await mycursor.close()
 
-    async def update_user_baby_class(self, user_id: int, baby_class: str) -> None:
+    async def update_user_baby_class(self, parent_id: int, baby_class: str) -> None:
         """ Updates the User Baby's class.
-        :param user_id: The ID of the baby's owner.
+        :param parent_id: The ID of one of the the baby's parent.
         :param baby_class: The new baby class to update to. """
 
         mycursor, db = await the_database()
-        await mycursor.execute("UPDATE UserBabies SET baby_class = %s WHERE user_id = %s", (baby_class, user_id))
+        await mycursor.execute("UPDATE UserBabies SET baby_class = %s WHERE parent_one = %s OR parent_two = %s", (baby_class, parent_id, parent_id))
         await db.commit()
         await mycursor.close()
 
-    async def delete_user_baby(self, user_id: int) -> None:
+    async def delete_user_baby(self, parent_id: int) -> None:
         """ Deletes the user's baby.
-        :param user_id: The ID of the baby's owner. """
+        :param parent_id: The ID of one of the baby's parent. """
 
         mycursor, db = await the_database()
-        await mycursor.execute("DELETE FROM UserBabies WHERE user_id = %s", (user_id,))
+        await mycursor.execute("DELETE FROM UserBabies WHERE parent_one = %s or parent_two = %s", (parent_id, parent_id))
         await db.commit()
         await mycursor.close()
 
