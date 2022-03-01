36b0c00ab7d1cd2d6a8bc6d61e53101e41948c86
Yago Michalak <yagomichalak@gmail.com>
2022-03-01 20:56:41 -0300

    ADDED pytest-asyncio to requirements.txt

--- tests/test_slc/test_handler.py
+++ tests/test_slc/test_handler.py
@@ -19,13 +19,15 @@ import cogs
 # from cogs.teacherfeedback import TeacherFeedback
 
 # SLC: Sloth Language Class
+# RUN: pytest --asyncio-mode=auto
 
 class TestSlothLanguageClass:
     """ Class for testing the features inside Sloth's
     Language Class system. """
 
-    @pytest.mark.skip(reason="Not implemented.")
-    def test_start_classroom(self) -> None:
+    # @pytest.mark.skip(reason="Not implemented.")
+    @pytest.mark.asyncio
+    async def test_start_classroom(self) -> None:
         """ Tests the creation of a classroom. """
 
         # TeacherFeedback.create_class = Mock()
