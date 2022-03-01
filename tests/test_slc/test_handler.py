09f780153753282208d50257500f7d51aafe7ca0
Yago Michalak <yagomichalak@gmail.com>
2022-03-01 12:56:13 -0300

    MADE placeholder for the tests to implement

--- tests/test_slc/test_handler.py
+++ tests/test_slc/test_handler.py
@@ -8,8 +8,11 @@ class TestSlothLanguageClass:
     """ Class for testing the features inside Sloth's
     Language Class system. """
 
-    @pytest.mark.skip(reason="Skipped for testing purposes.")
-    def test_one_equals_one(self) -> None:
-        """ Testing test that checks whether 1 equals to 1. """
+    @pytest.mark.skip(reason="Not implemented.")
+    def test_start_classroom(self) -> None: pass
 
-        assert 1 == 1
+    @pytest.mark.skip(reason="Not implemented.")
+    def test_end_classroom(self) -> None: pass
+
+    @pytest.mark.skip(reason="Not implemented.")
+    def test_reward_people(self) -> None: pass
