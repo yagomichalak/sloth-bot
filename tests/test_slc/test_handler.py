f14f18e31fe7464c28bac53673ce6e9df397b7e4
Yago Michalak <yagomichalak@gmail.com>
2022-03-01 14:20:36 -0300

    ADDED a users.json

--- tests/test_slc/test_handler.py
+++ tests/test_slc/test_handler.py
@@ -1,6 +1,10 @@
 import pytest
 from unittest.mock import Mock
 import os
+from typing import List, Dict, Union, Optional
+from tests.data import users
+
+from cogs.teacherfeedback import TeacherFeedback
 
 # SLC: Sloth Language Class
 
@@ -9,10 +13,15 @@ class TestSlothLanguageClass:
     Language Class system. """
 
     @pytest.mark.skip(reason="Not implemented.")
-    def test_start_classroom(self) -> None: pass
+    def test_start_classroom(self) -> None:
+        """ Tests the creation of a classroom. """
+
+        # TeacherFeedback.create_class = Mock()
+        pass
 
     @pytest.mark.skip(reason="Not implemented.")
     def test_end_classroom(self) -> None: pass
 
     @pytest.mark.skip(reason="Not implemented.")
     def test_reward_people(self) -> None: pass
+
