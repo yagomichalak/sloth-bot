bc842c8755602d444168088972fd5b0d056d75f2
Yago Michalak <yagomichalak@gmail.com>
2022-03-04 19:59:27 -0300

    MADE a conftest.py and a utils for the tests

--- tests/test_slc/test_handler.py
+++ tests/test_slc/test_handler.py
@@ -6,16 +6,24 @@ from unittest.mock import Mock
 
 from typing import List, Dict, Union, Optional
 
-# Appends the path of folders 2 folders back, so you can import them
-sys.path.append(
-    os.path.join(
-        os.path.dirname(
-            os.path.realpath(__file__)
-        ), os.pardir, os.pardir
-    )
-)
 
-import cogs
+
+# Appends the path of folders 2 folders back, so you can import them
+# sys.path.append(
+#     os.path.join(
+#         os.path.dirname(
+#             os.path.realpath(__file__)
+#         ), os.pardir, os.pardir
+#     )
+# )
+# print(    os.path.join(
+#         os.path.dirname(
+#             os.path.realpath(__file__)
+#         ), os.pardir, os.pardir
+#     ))
+
+
+# import cogs
 # from cogs.teacherfeedback import TeacherFeedback
 
 # SLC: Sloth Language Class
@@ -25,13 +33,20 @@ class TestSlothLanguageClass:
     """ Class for testing the features inside Sloth's
     Language Class system. """
 
+    
+
     # @pytest.mark.skip(reason="Not implemented.")
     @pytest.mark.asyncio
     async def test_start_classroom(self) -> None:
         """ Tests the creation of a classroom. """
 
-        # TeacherFeedback.create_class = Mock()
-        pass
+
+        from cogs import teacherfeedback
+
+        # teacherfeedback.TeacherFeedback.create_class(member)
+        
+        #  = Mock()
+
 
     @pytest.mark.skip(reason="Not implemented.")
     def test_end_classroom(self) -> None: pass
