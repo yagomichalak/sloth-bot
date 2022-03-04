a5223ac17c0343d965642d188f85c8e76c8f2caa
Yago Michalak <yagomichalak@gmail.com>
2022-03-04 20:18:56 -0300

    MADE a few changes in the confest and DB connection

--- tests/test_slc/test_handler.py
+++ tests/test_slc/test_handler.py
@@ -3,37 +3,51 @@ import sys
 
 import pytest
 from unittest.mock import Mock
+from typing import List, Dict, Union, Optional, Any
 
-from typing import List, Dict, Union, Optional
-
-
-
-# Appends the path of folders 2 folders back, so you can import them
-# sys.path.append(
-#     os.path.join(
-#         os.path.dirname(
-#             os.path.realpath(__file__)
-#         ), os.pardir, os.pardir
-#     )
-# )
-# print(    os.path.join(
-#         os.path.dirname(
-#             os.path.realpath(__file__)
-#         ), os.pardir, os.pardir
-#     ))
-
-
-# import cogs
-# from cogs.teacherfeedback import TeacherFeedback
+from .conftest import (
+    DatabaseDriver,
+    Query,
+    Result,
+    get_users,
+    _INSERT,
+    createConnection
+)
 
 # SLC: Sloth Language Class
-# RUN: pytest --asyncio-mode=auto
+# RUN: pytest --asyncio-mode=auto --capture=no
 
 class TestSlothLanguageClass:
     """ Class for testing the features inside Sloth's
     Language Class system. """
 
-    
+    def setup(self) -> None:
+        """ Setup of the test class, which creates a database connection, cache etc. """
+
+        cursor, connection = createConnection()
+
+        refer = DatabaseDriver.execute
+
+        def execute(self: DatabaseDriver, query: Union[str, Query]) -> Result:
+            """ Executes a query using the database driver. """
+
+            print("execute", query)
+
+            assert isinstance(query, Query), \
+                "Unexpected raw query execution"
+
+            result = refer(self, query)
+
+            if query.kind == _INSERT and result.commited:
+                model = result.asdict()
+
+            return result
+
+        DatabaseDriver.execute = execute
+        self.driver = DatabaseDriver(cursor, connection)
+
+        self.users: List[Dict[str, Any]] = get_users()
+
 
     # @pytest.mark.skip(reason="Not implemented.")
     @pytest.mark.asyncio
