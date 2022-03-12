3e120602793bdc51f064d2c8649da8a2946b4fe8
Yago Michalak <yagomichalak@gmail.com>
2022-03-12 13:43:06 -0300

    FIXED & IMPROVED the modrep system

--- cogs/modactivity.py
+++ cogs/modactivity.py
@@ -6,6 +6,7 @@ import asyncio
 import os
 from extra import utils
 from extra.moderation.modactivity import ModActivityTable
+from extra.prompt.menu import ConfirmButton
 
 senior_mod_role_id: int = int(os.getenv('SENIOR_MOD_ROLE_ID', 123))
 mod_role_id = int(os.getenv('MOD_ROLE_ID', 123))
@@ -46,70 +47,64 @@ class ModActivity(ModActivityTable):
         moderator_role = discord.utils.get(guild.roles, id=mod_role_id)
         if moderator_role not in member.roles:
             return
-        epoch = datetime.utcfromtimestamp(0)
-        the_time = (datetime.utcnow() - epoch).total_seconds()
-        old_time = await self.get_moderator_current_timestamp(member.id, int(the_time))
-        addition = the_time - old_time
+            
+        current_ts = await utils.get_timestamp()
+        old_time = await self.get_moderator_current_timestamp(member.id, current_ts)
+        addition = current_ts - old_time
 
         if not before.channel:
-            return await self.update_moderator(member.id)
+            return await self.update_moderator_timestamp(member.id)
 
         if not after.channel:
-            await self.add_time_moderator(member.id, addition)
+            await self.update_moderator_time(member.id, addition)
 
 
     @utils.is_allowed([senior_mod_role_id], throw_exc=True)
     @commands.command(aliases=['mods_reputation', 'mod_rep'])
+    @commands.cooldown(1, 5, commands.BucketType.user)
     async def modrep(self, ctx):
         """ (STAFF) Shows all the moderators and their statuses in an embedded message. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute('SELECT * FROM ModActivity')
-        mods = await mycursor.fetchall()
-        await mycursor.close()
+        mod_activities = await self.get_mod_activities()
 
-        embed = discord.Embed(title="This Week's Report",
-                              description="Moderators' activity statuses.",
-                              url='https://discordapp.com',
-                              colour=discord.Colour.dark_green(), timestamp=ctx.message.created_at)
-        embed.set_footer(text='Activity Report',
-                         icon_url=self.client.user.display_avatar)
-        embed.set_thumbnail(
-            url='https://cdn.discordapp.com/icons/459195345419763713/a_866e6fe442bfb651353df72826238f54.jpg')
+        member: discord.Member = ctx.author
+
+        embed = discord.Embed(
+            title="This Week's Report", description="Moderators' activity statuses.",
+            url='https://discordapp.com', color=discord.Color.dark_green(), 
+            timestamp=ctx.message.created_at)
+        embed.set_footer(text='Activity Report', icon_url=self.client.user.display_avatar)
+        embed.set_thumbnail(url=ctx.guild.icon.url)
         embed.set_author(name=ctx.guild.name)
-        for mod in mods:
+        for mod in mod_activities:
             m, s = divmod(mod[1], 60)
             h, m = divmod(m, 60)
             user = discord.utils.get(ctx.guild.members, id=mod[0])
-            embed.add_field(name=f'👤__**{user}**__',
-                            value=f"**- Voice Chat Activity:**\n{h:d} hours, {m:02d} minutes and {s:02d} seconds\n**- Text Chat Activity:**\n{mod[3]} messages",
-                            inline=False)
+            embed.add_field(
+                name=f'👤__**{user}**__',
+                value=f"**- Voice Chat Activity:**\n{h:d} hours, {m:02d} minutes and {s:02d} seconds\n**- Text Chat Activity:**\n{mod[3]} messages",
+                inline=False)
+
+        confirm_view: discord.ui.View = ConfirmButton(member, timeout=60)
 
         await ctx.send(embed=embed)
-        the_msg = await ctx.send("**Do you want to reset the data?**")
-        await the_msg.add_reaction('✅')
-        await the_msg.add_reaction('❌')
+        msg = await ctx.send("**Do you want to reset the data?**", view=confirm_view)
+        await confirm_view.wait()
 
-        def check(reaction, user):
-            return user == ctx.message.author and str(reaction.emoji) in '✅❌'
+        await utils.disable_buttons(confirm_view)
+        await msg.edit(view=confirm_view)
 
-        try:
-            reaction, user = await self.client.wait_for('reaction_add', timeout=60.0, check=check)
-        except asyncio.TimeoutError:
-            timeout = await ctx.send("**Timeout, not deleting it!**")
-            await the_msg.delete()
-            await asyncio.sleep(2)
-            return await timeout.delete()
+        if confirm_view.value is None:
+            await ctx.send(f"**Timeout, not deleting it, {member.mention}!**", delete_after=3)
+        elif confirm_view.value:
+            await self.delete_mod_activity()
+            await ctx.send(f"**Mod Activity data reset, {member.mention}!**", delete_after=3)
         else:
-            if str(reaction.emoji) == "✅":
-                await self.reset_table_mods(ctx)
-                resp = await ctx.send("**Log reset!**")
-            else:
-                resp = await ctx.send("**Not reset!**")
-            await the_msg.delete()
-            await asyncio.sleep(2)
-            return await resp.delete()
+            await ctx.send(f"**Not deleting it then, {member.mention}!**", delete_after=3)
 
+        await msg.delete()
+
+                
 
 def setup(client):
     client.add_cog(ModActivity(client))
