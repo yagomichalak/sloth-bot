9bdb4b8fda815226c7ce3443ca73d156287a55e4
Yago Michalak <yagomichalak@gmail.com>
2022-03-23 20:29:17 -0300

    FIXED MakeDumps event

--- cogs/tools.py
+++ cogs/tools.py
@@ -89,12 +89,12 @@ class Tools(*tool_cogs):
 			current_ts=int(current_ts), event_label="make_dumps", ad_time=43200): # Every 12 hours
 
 			# Updates time and makes the dumps.
-			await Communication.update_advertising_time(event_label="make_dumps", current_ts=current_ts)
-
 			guild = self.client.get_guild(int(os.getenv("SERVER_ID")))
 			dumps_channel = discord.utils.get(guild.channels, id=int(os.getenv("DUMPS_CHANNEL_ID")))
-
-			await self.make_dump_callback(channel=dumps_channel)
+			try:
+				await self.make_dump_callback(channel=dumps_channel)
+			finally:
+				await Communication.update_advertising_time(event_label="make_dumps", current_ts=current_ts)
 
 
 	@commands.Cog.listener()
@@ -1404,7 +1404,7 @@ class Tools(*tool_cogs):
 			await ctx.send(f"**{member.mention}, is back home, after a long day of surfing,!**")
 
 	@commands.command(aliases=["make_dump", "mkdps", "mkdp"])
-	# @commands.cooldown(1, 15, commands.BucketType.guild)
+	@commands.cooldown(1, 15, commands.BucketType.guild)
 	@commands.has_permissions(administrator=True)
 	async def make_dumps(self, ctx) -> None:
 		""" Makes dumps of the databases and posts them in a specific channel. """
@@ -1414,9 +1414,11 @@ class Tools(*tool_cogs):
 		dumps_channel = discord.utils.get(ctx.guild.channels, id=int(os.getenv("DUMPS_CHANNEL_ID")))
 
 		Communication = self.client.get_cog('Communication')
-		await self.make_dump_callback(dumps_channel)
-		await Communication.update_advertising_time(event_label="make_dumps", current_ts=current_ts)
-		await ctx.send(f"**Posted dumps in {dumps_channel.mention}, {member.mention}!**")
+		try:
+			await self.make_dump_callback(dumps_channel)
+			await ctx.send(f"**Posted dumps in {dumps_channel.mention}, {member.mention}!**")
+		finally:
+			await Communication.update_advertising_time(event_label="make_dumps", current_ts=current_ts)
 
 	async def make_dump_callback(self, channel: Union[discord.TextChannel, discord.Thread]) -> None:
 		""" Callback for the make dumps command.
@@ -1439,21 +1441,22 @@ class Tools(*tool_cogs):
 		file_path: str = f'media/temporary/sloth_db_dump_{d}_{m}_{y}_{h}_{m}.sql'
 		file_path2: str = f'media/temporary/slothdjango_db_dump_{d}_{m}_{y}_{h}_{m}.sql'
 		file_path3: str = f'media/temporary/.env_schema_{d}_{m}_{y}_{h}_{m}.txt'
+		try:
+			command = f"mysqldump -u {os.getenv('SLOTH_DB_USER')} -p{os.getenv('SLOTH_DB_PASSWORD')} {os.getenv('SLOTH_DB_NAME')}" \
+				f" > {file_path}"
+			subprocess.call(command.split(), shell=True)
 
-		command = f"mysqldump -u {os.getenv('SLOTH_DB_USER')} -p{os.getenv('SLOTH_DB_PASSWORD')} {os.getenv('SLOTH_DB_NAME')}" \
-			f" > {file_path}"
-		subprocess.getstatusoutput(command)
+			command2 = f"mysqldump -u {os.getenv('DJANGO_DB_USER')} -p{os.getenv('DJANGO_DB_PASSWORD')} {os.getenv('DJANGO_DB_NAME')}" \
+				f" > {file_path2}"
+			subprocess.call(command2.split(), shell=True)
 
-		command2 = f"mysqldump -u {os.getenv('DJANGO_DB_USER')} -p{os.getenv('DJANGO_DB_PASSWORD')} {os.getenv('DJANGO_DB_NAME')}" \
-			f" > {file_path2}"
-		subprocess.getstatusoutput(command2)
+			with open(file_path3, 'w', encoding="utf-8") as f3:
+				f3.writelines('\n'.join(map(lambda key: f"{key} = 123", os.environ.__dict__['_data'].keys())))
 
-		with open(file_path3, 'w', encoding="utf-8") as f3:
-			f3.writelines('\n'.join(map(lambda key: f"{key} = 123", os.environ.__dict__['_data'].keys())))
-
-		# Posts it
-		await channel.send(files=[discord.File(file_path), discord.File(file_path2), discord.File(file_path3)])
-		os.remove(file_path); os.remove(file_path2); os.remove(file_path3)
+			# Posts it
+			await channel.send(files=[discord.File(file_path), discord.File(file_path2), discord.File(file_path3)])
+		finally:
+			os.remove(file_path); os.remove(file_path2); os.remove(file_path3)
 
 
 def setup(client):
