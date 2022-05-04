98bb869908235360d360dba91ad90561afec814a
Yago Michalak <yagomichalak@gmail.com>
2022-05-04 09:30:36 -0300

    FIXED attain_grace for users who are not married

--- extra/slothclasses/seraph.py
+++ extra/slothclasses/seraph.py
@@ -467,7 +467,7 @@ class Seraph(Player):
         if target_sloth_profile[1] == 'default':
             return await ctx.send(f"**You cannot attain the grace for someone who has a `default` Sloth class, {perpetrator.mention}!**")
 
-        marriage = await self.client.get_cog('SlothClass').get_user_marriage(perpetrator.id)
+        marriage = await self.client.get_cog('SlothClass').get_user_marriage(target.id)
         if not marriage['partner']:
             return await ctx.send(f"**You cannot attain the grace for someone who is not married, {perpetrator.mention}!**")
 
