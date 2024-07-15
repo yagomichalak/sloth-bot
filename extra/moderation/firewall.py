f0f76f2dcf855fbfaf86709b721c58546317c44b
Yago <yagomichalak@gmail.com>
2024-07-16 01:18:31 +0200

    refactor: Changes the Duolingo, BypassFirewall & ModerationFirewall cogs to use DatabaseCore for database queries

--- extra/moderation/firewall.py
+++ extra/moderation/firewall.py
@@ -1,7 +1,7 @@
-import discord
 from discord.ext import commands
-from mysqldb import the_database
+from mysqldb import DatabaseCore
 from typing import List
+
 
 class BypassFirewallTable(commands.Cog):
     """ Category for the BypassFirewall table in the database. """
@@ -10,7 +10,7 @@ class BypassFirewallTable(commands.Cog):
         """ Class init method. """
 
         self.client = client
-
+        self.db = DatabaseCore()
 
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
@@ -21,15 +21,12 @@ class BypassFirewallTable(commands.Cog):
             return await ctx.send("**Table __BypassFirewall__ already exists!**")
 
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("""
+        await self.db.execute_query("""
             CREATE TABLE BypassFirewall (
                 user_id BIGINT NOT NULL,
                 PRIMARY KEY (user_id)
             )""")
-        await mycursor.execute("INSERT INTO Firewall VALUES(0)")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("INSERT INTO Firewall VALUES(0)")
 
         return await ctx.send("**Table __BypassFirewall__ created!**", delete_after=3)
 
@@ -41,10 +38,7 @@ class BypassFirewallTable(commands.Cog):
         if not await self.check_table_bypass_firewall_exists():
             return await ctx.send("**Table __BypassFirewall__ doesn't exist!**")
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("DROP TABLE BypassFirewall")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DROP TABLE BypassFirewall")
 
         return await ctx.send("**Table __BypassFirewall__ dropped!**", delete_after=3)
 
@@ -57,10 +51,7 @@ class BypassFirewallTable(commands.Cog):
             return await ctx.send("**Table __BypassFirewall__ doesn't exist yet**")
 
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("DELETE FROM BypassFirewall")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DELETE FROM BypassFirewall")
 
         return await ctx.send("**Table __BypassFirewall__ reset!**", delete_after=3)
 
@@ -68,56 +59,33 @@ class BypassFirewallTable(commands.Cog):
     async def check_table_bypass_firewall_exists(self) -> bool:
         """ Checks if the BypassFirewall table exists """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SHOW TABLE STATUS LIKE 'BypassFirewall'")
-        table_info = await mycursor.fetchall()
-        await mycursor.close()
-
-        if len(table_info) == 0:
-            return False
-
-        else:
-            return True
+        return await self.db.table_exists("BypassFirewall")
 
     # ===== INSERT =====
     async def insert_bypass_firewall_user(self, user_id: int) -> None:
         """ Inserts a user into the BypassFirewall table.
         :param user_id: The ID of the user to insert. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("INSERT INTO BypassFirewall (user_id) VALUES (%s)", (user_id,))
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("INSERT INTO BypassFirewall (user_id) VALUES (%s)", (user_id,))
 
     # ===== SELECT =====
     async def get_bypass_firewall_user(self, user_id: int) -> List[int]:
         """ Gets a user from the BypassFirewall table.
         :param user_id: The ID of the user to get. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SELECT * FROM BypassFirewall WHERE user_id = %s", (user_id,))
-        bf_user = await mycursor.fetchone()
-        await mycursor.close()
-        return bf_user
+        return await self.db.execute_query("SELECT * FROM BypassFirewall WHERE user_id = %s", (user_id,), fetch="one")
 
     async def get_bypass_firewall_users(self) -> List[List[int]]:
         """ Gets all users from the BypassFirewall table. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SELECT * FROM BypassFirewall")
-        bf_users = await mycursor.fetchall()
-        await mycursor.close()
-        return bf_users
+        return await self.db.execute_query("SELECT * FROM BypassFirewall", fetch="all")
 
     # ===== DELETE =====
     async def delete_bypass_firewall_user(self, user_id: int) -> None:
         """ Deletes a user from the BypassFirewall table.
         :param user_id: The ID of the user to delete. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("DELETE FROM BypassFirewall WHERE user_id = %s", (user_id,))
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DELETE FROM BypassFirewall WHERE user_id = %s", (user_id,))
     
 
 class ModerationFirewallTable(commands.Cog):
@@ -125,6 +93,7 @@ class ModerationFirewallTable(commands.Cog):
 
     def __init__(self, client) -> None:
         self.client = client
+        self.db = DatabaseCore()
 
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
@@ -135,12 +104,9 @@ class ModerationFirewallTable(commands.Cog):
             return await ctx.send("**Table __Firewall__ already exists!**")
 
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("""CREATE TABLE Firewall (
+        await self.db.execute_query("""CREATE TABLE Firewall (
             state TINYINT(1) NOT NULL DEFAULT 0)""")
-        await mycursor.execute("INSERT INTO Firewall VALUES(0)")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("INSERT INTO Firewall VALUES(0)")
 
         return await ctx.send("**Table __Firewall__ created!**", delete_after=3)
 
@@ -152,10 +118,7 @@ class ModerationFirewallTable(commands.Cog):
         if not await self.check_table_firewall_exists():
             return await ctx.send("**Table __Firewall__ doesn't exist!**")
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("DROP TABLE Firewall")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DROP TABLE Firewall")
 
         return await ctx.send("**Table __Firewall__ dropped!**", delete_after=3)
 
@@ -168,44 +131,24 @@ class ModerationFirewallTable(commands.Cog):
             return await ctx.send("**Table __Firewall__ doesn't exist yet**")
 
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("DELETE FROM Firewall")
-        await mycursor.execute("INSERT INTO Firewall VALUES(0)")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DELETE FROM Firewall")
+        await self.db.execute_query("INSERT INTO Firewall VALUES(0)")
 
         return await ctx.send("**Table __Firewall__ reset!**", delete_after=3)
 
     async def check_table_firewall_exists(self) -> bool:
         """ Checks if the Firewall table exists """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SHOW TABLE STATUS LIKE 'Firewall'")
-        table_info = await mycursor.fetchall()
-        await mycursor.close()
-
-        if len(table_info) == 0:
-            return False
-
-        else:
-            return True
+        return await self.db.table_exists("Firewall")
 
     async def set_firewall_state(self, state: int) -> None:
         """ Sets the firewall state to either true or false. 
         :param state: The state of the firewall to set. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("UPDATE Firewall SET state = %s", (state,))
-        await db.commit()
-        await mycursor.close()
-
+        await self.db.execute_query("UPDATE Firewall SET state = %s", (state,))
 
     async def get_firewall_state(self) -> int:
         """ Gets the firewall's current state. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SELECT state FROM Firewall")
-        fw_state = await mycursor.fetchone()
-        await mycursor.close()
-        return fw_state[0]
+        return await self.db.execute_query("SELECT state FROM Firewall", fetch="one")
 
