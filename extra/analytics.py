f9341d74f397bc6d6ae6a0ba9530add47e9598c0
Yago <yagomichalak@gmail.com>
2024-07-15 23:26:11 +0200

    refactor: Changes the Analytics & MembersScore cogs to use DatabaseCore for database queries

--- extra/analytics.py
+++ extra/analytics.py
@@ -1,8 +1,9 @@
 from discord.ext import commands
-from mysqldb import the_database
+from mysqldb import DatabaseCore
 from typing import List, Union
 from extra import utils
 from datetime import datetime
+
 
 class SlothAnalyticsTable(commands.Cog):
     """ Class for managing the SlothAnalytics table in the database. """
@@ -11,6 +12,7 @@ class SlothAnalyticsTable(commands.Cog):
         """ Class init method. """
 
         self.client = client
+        self.db = DatabaseCore()
 
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
@@ -18,19 +20,15 @@ class SlothAnalyticsTable(commands.Cog):
         """ (ADM) Creates the SlothAnalytics table. """
 
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute(
+        await self.db.execute_query(
             """CREATE TABLE SlothAnalytics (
                 m_joined INT DEFAULT 0,
                 m_left INT DEFAULT 0, 
                 messages_sent INT DEFAULT 0,
                 day_now VARCHAR(2) DEFAULT NULL
             )""")
-        await db.commit()
         time_now = await utils.get_time_now()
-        await mycursor.execute("INSERT INTO SlothAnalytics (day_now) VALUES (%s)", (time_now.day))
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("INSERT INTO SlothAnalytics (day_now) VALUES (%s)", (time_now.day))
         return await ctx.send("**Table *SlothAnalytics* created!**", delete_after=3)
 
     @commands.command(hidden=True)
@@ -39,10 +37,7 @@ class SlothAnalyticsTable(commands.Cog):
         """ (ADM) Drops the SlothAnalytics table. """
 
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("DROP TABLE SlothAnalytics")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DROP TABLE SlothAnalytics")
         return await ctx.send("**Table *SlothAnalytics* dropped!**", delete_after=3)
 
     @commands.command(hidden=True)
@@ -57,54 +52,36 @@ class SlothAnalyticsTable(commands.Cog):
     async def reset_table_sloth_analytics_callback(self) -> None:
         """ (ADM) Resets the SlothAnalytics table. (Callback) """
 
-        mycursor, db = await the_database()
         time_now = await utils.get_time_now()
-        await mycursor.execute("UPDATE SlothAnalytics SET day_now = %s, m_joined = 0, m_left = 0, messages_sent = 0", (time_now.day,))
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("UPDATE SlothAnalytics SET day_now = %s, m_joined = 0, m_left = 0, messages_sent = 0", (time_now.day,))
 
     async def update_joined(self) -> None:
         """ Updates the joined members counting. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("UPDATE SlothAnalytics SET m_joined = m_joined + 1")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("UPDATE SlothAnalytics SET m_joined = m_joined + 1")
 
     async def update_left(self) -> None:
         """ Updates the left members counting. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("UPDATE SlothAnalytics SET m_left = m_left + 1")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("UPDATE SlothAnalytics SET m_left = m_left + 1")
 
     async def update_messages(self) -> None:
         """ Updates the message counting. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("UPDATE SlothAnalytics SET messages_sent = messages_sent + 1")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("UPDATE SlothAnalytics SET messages_sent = messages_sent + 1")
 
     async def update_day(self, day: str) -> None:
         """ Updates the day.
         :param day: The day it is. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("UPDATE SlothAnalytics SET day_now = %s", (day,))
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("UPDATE SlothAnalytics SET day_now = %s", (day,))
 
     async def check_relatory_time(self, time_now: str) -> bool:
         """ Checks the relatory time in the database, so it knows whether it should reset
         the data or not.
         :param time_now: The current time. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SELECT * from SlothAnalytics")
-        info = await mycursor.fetchone()
-        await mycursor.close()
+        info = await self.db.execute_query("SELECT * from SlothAnalytics", fetch="one")
         if info and str(info[3]) != str(time_now):
             return True
         else:
@@ -113,11 +90,7 @@ class SlothAnalyticsTable(commands.Cog):
     async def get_info(self) -> List[int]:
         """ Gets the analytics info. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SELECT * from SlothAnalytics")
-        info = await mycursor.fetchone()
-        await mycursor.close()
-        return info
+        return await self.db.execute_query("SELECT * from SlothAnalytics", fetch="one")
 
 
 class DataBumpsTable(commands.Cog):
@@ -136,8 +109,7 @@ class DataBumpsTable(commands.Cog):
         if await self.table_data_bumps_exists():
             return await ctx.send("**The table `DataBumps` already exists!**")
 
-        mycursor, db = await the_database()
-        await mycursor.execute("""
+        await self.db.execute_query("""
             CREATE TABLE DataBumps (
                 m_joined BIGINT DEFAULT 0,
                 m_left BIGINT DEFAULT 0, 
@@ -146,8 +118,6 @@ class DataBumpsTable(commands.Cog):
                 online BIGINT DEFAULT 0, 
                 complete_date VARCHAR(11) DEFAULT NULL
             )""")
-        await db.commit()
-        await mycursor.close()
         return await ctx.send("**Table `DataBumps` created!**")
 
     @commands.command(hidden=True)
@@ -158,10 +128,7 @@ class DataBumpsTable(commands.Cog):
         if not await self.table_data_bumps_exists():
             return await ctx.send("**The table `DataBumps` doesn't exist!**")
 
-        mycursor, db = await the_database()
-        await mycursor.execute("DROP TABLE DataBumps")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DROP TABLE DataBumps")
         return await ctx.send("**Table `DataBumps` dropped!**")
 
     @commands.command(hidden=True)
@@ -172,23 +139,13 @@ class DataBumpsTable(commands.Cog):
         if not await self.table_data_bumps_exists():
             return await ctx.send("**The table `DataBumps` doesn't exist yet!**")
 
-        mycursor, db = await the_database()
-        await mycursor.execute("DELETE FROM DataBumps")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DELETE FROM DataBumps")
         await ctx.send("**Table `DataBumps` reset!**")
 
     async def table_data_bumps_exists(self) -> bool:
         """ Checks whether the DataBumps table exists. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SHOW TABLE STATUS LIKE 'DataBumps'")
-        exists = await mycursor.fetchone()
-        await mycursor.close()
-        if exists:
-            return True
-        else:
-            return False
+        return await self.db.table_exists("DataBumps")
 
     async def bump_data(self, 
         joined: int, left: int, messages: int, 
@@ -201,67 +158,53 @@ class DataBumpsTable(commands.Cog):
         :param online: The online members counter.
         :param complete_date: The complete date referring to the data. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute('''
+        await self.db.execute_query('''
             INSERT INTO DataBumps (
             m_joined, m_left, messages, members, online, complete_date)
             VALUES (%s, %s, %s, %s, %s, %s)''', (joined, left, messages, members, online, complete_date))
-        await db.commit()
-        await mycursor.close()
 
     async def get_monthly_total(self) -> List[int]:
         """ Gets all monthly total of members. """
 
-        mycursor, _ = await the_database()
 
-        await mycursor.execute("""
+        months = await self.db.execute_query("""
             SELECT Month(STR_TO_DATE(complete_date, '%d/%m/%Y')) AS Months,
                 (SUM(m_joined) - SUM(m_left)) AS 'Totals Joinings',
                 COUNT(*) AS 'Records',
                 MAX(members) AS 'Members at the End'
             FROM DataBumps
             GROUP BY Months;
-        """)
+        """, fetch="all")
 
-        months = await mycursor.fetchall()
         months = list(map(lambda e: e[3], months))
-        await mycursor.close()
         return months
 
     async def get_daily_total(self) -> List[int]:
         """ Gets all daily total of members. """
 
-        mycursor, _ = await the_database()
 
-        await mycursor.execute("""
+        months = await self.db.execute_query("""
             SELECT STR_TO_DATE(complete_date, '%d/%m/%Y') AS Days,
                 (SUM(m_joined) - SUM(m_left)) AS 'Totals Joinings',
                 COUNT(*) AS 'Records',
                 members AS 'Members at the End'
             FROM DataBumps
             GROUP BY Days;
-        """)
+        """, fetch="all")
 
-        months = await mycursor.fetchall()
         months = list(map(lambda e: e[3], months))
-        await mycursor.close()
         return months
 
     async def get_last_members_record(self) -> int:
         """ Gets the last record of total members. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SELECT members FROM DataBumps ORDER BY members DESC LIMIT 1")
-        last_record = await mycursor.fetchone()
-        await mycursor.close()
+        last_record = await self.db.execute_query("SELECT members FROM DataBumps ORDER BY members DESC LIMIT 1", fetch="one")
         return 0 if not last_record else last_record[0]
-
 
     async def get_month_statuses(self) -> List[Union[datetime, str, int]]:
         """ Gets months statuses. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("""
+        return await self.db.execute_query("""
             SELECT
                 STR_TO_DATE(complete_date, '%d/%m/%Y') AS Months,
                 SUM(m_joined) - SUM(m_left) AS 'Total Joins',
@@ -269,8 +212,4 @@ class DataBumpsTable(commands.Cog):
                 MAX(members) AS 'Last Member Record of the Month'
             FROM DataBumps
             GROUP BY YEAR(Months), MONTH(Months)
-        """)
-
-        months = await mycursor.fetchall()
-        await mycursor.close()
-        return months
+        """, fetch="all")
