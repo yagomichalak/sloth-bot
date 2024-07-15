b9fe563377fb5d2c9b1b99d85e5a6ff8dd5cd9c6
Yago <yagomichalak@gmail.com>
2024-07-16 01:25:55 +0200

    refactor: Changes the MemberReminder & ModerationMutedMember cogs to use DatabaseCore for database queries

--- extra/misc/reminder.py
+++ extra/misc/reminder.py
@@ -1,7 +1,7 @@
-import discord
 from discord.ext import commands
-from mysqldb import the_database
+from mysqldb import DatabaseCore
 from typing import List, Union
+
 
 class MemberReminderTable(commands.Cog):
     """ Class for managing the MemberReminder table in the database. """
@@ -10,6 +10,7 @@ class MemberReminderTable(commands.Cog):
         """ Class init method. """
 
         self.client = client
+        self.db = DatabaseCore()
 
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
@@ -20,8 +21,7 @@ class MemberReminderTable(commands.Cog):
             return await ctx.send("**Table __MemberReminder__ already exists!**")
 
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("""CREATE TABLE MemberReminder (
+        await self.db.execute_query("""CREATE TABLE MemberReminder (
             reminder_id BIGINT NOT NULL AUTO_INCREMENT,
             user_id BIGINT NOT NULL,
             text VARCHAR(100) NOT NULL,
@@ -29,8 +29,6 @@ class MemberReminderTable(commands.Cog):
             remind_in BIGINT NOT NULL,
             PRIMARY KEY (reminder_id)
             ) """)
-        await db.commit()
-        await mycursor.close()
 
         return await ctx.send("**Table __MemberReminder__ created!**", delete_after=3)
 
@@ -43,10 +41,7 @@ class MemberReminderTable(commands.Cog):
             return await ctx.send("**Table __MemberReminder__ doesn't exist!**")
 
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("DROP TABLE MemberReminder")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DROP TABLE MemberReminder")
 
         return await ctx.send("**Table __MemberReminder__ dropped!**", delete_after=3)
 
@@ -59,25 +54,14 @@ class MemberReminderTable(commands.Cog):
             return await ctx.send("**Table __MemberReminder__ doesn't exist yet!**")
 
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("DELETE FROM MemberReminder")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DELETE FROM MemberReminder")
 
         return await ctx.send("**Table __MemberReminder__ reset!**", delete_after=3)
 
     async def check_table_member_reminder(self) -> bool:
         """ Checks if the MemberReminder table exists """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SHOW TABLE STATUS LIKE 'MemberReminder'")
-        table_info = await mycursor.fetchone()
-        await mycursor.close()
-
-        if table_info:
-            return True
-        else:
-            return False
+        return await self.db.table_exists("MemberReminder")
 
     async def insert_member_reminder(self, user_id: int, text: str, reminder_timestamp: int, remind_in: int) -> None:
         """ Inserts an entry concerning the user's last seen datetime.
@@ -86,48 +70,31 @@ class MemberReminderTable(commands.Cog):
         :param reminder_timestamp: The current timestamp.
         :param remind_in: The amount of seconds to wait until reminding the user. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("""
+        await self.db.execute_query("""
         INSERT INTO MemberReminder (user_id, text, reminder_timestamp, remind_in) 
         VALUES (%s, %s, %s, %s)""", (user_id, text, reminder_timestamp, remind_in))
-        await db.commit()
-        await mycursor.close()
 
     async def get_member_reminders(self, user_id: int) -> List[List[Union[str, int]]]:
         """ Gets the user's reminders.
         :param user_id: The ID of the user. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SELECT * FROM MemberReminder WHERE user_id = %s", (user_id,))
-        reminders = await mycursor.fetchall()
-        await mycursor.close()
-        return reminders
+        return await self.db.execute_query("SELECT * FROM MemberReminder WHERE user_id = %s", (user_id,), fetch="all")
 
     async def get_reminder(self, reminder_id: int) -> List[Union[str, int]]:
         """ Gets a reminder by ID.
         :param reminder_id: The reminder ID. """
         
-        mycursor, _ = await the_database()
-        await mycursor.execute("SELECT * FROM MemberReminder WHERE reminder_id = %s", (reminder_id,))
-        reminder = await mycursor.fetchone()
-        await mycursor.close()
-        return reminder
+        return await self.db.execute_query("SELECT * FROM MemberReminder WHERE reminder_id = %s", (reminder_id,), fetch="one")
 
     async def get_due_reminders(self, current_ts: int) -> List[int]:
         """ Gets reminders that are due.. 
         :param current_ts: The current timestamp. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SELECT * FROM MemberReminder WHERE (%s -  reminder_timestamp) >= remind_in", (current_ts,))
-        reminders = [(m[0], m[1], m[2]) for m in await mycursor.fetchall()]
-        await mycursor.close()
-        return reminders
+        reminders = await self.db.execute_query("SELECT * FROM MemberReminder WHERE (%s -  reminder_timestamp) >= remind_in", (current_ts,), fetch="all")
+        return [(m[0], m[1], m[2]) for m in await reminders]
 
     async def delete_member_reminder(self, reminder_id: int) -> None:
         """ Updates the user's last seen datetime.
         :param reminder_id: The ID of the reminder to delete. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("DELETE FROM MemberReminder WHERE reminder_id = %s", (reminder_id,))
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DELETE FROM MemberReminder WHERE reminder_id = %s", (reminder_id,))
--- extra/moderation/mutedmember.py
+++ extra/moderation/mutedmember.py
@@ -1,13 +1,13 @@
-from datetime import time
-import discord
 from discord.ext import commands
-from mysqldb import the_database
+from mysqldb import DatabaseCore
 from typing import List, Optional, Tuple
+
 
 class ModerationMutedMemberTable(commands.Cog):
     
     def __init__(self, client) -> None:
         self.client = client
+        self.db = DatabaseCore()
 
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
@@ -18,16 +18,13 @@ class ModerationMutedMemberTable(commands.Cog):
             return await ctx.send("**Table __MutedMember__ already exists!**")
 
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("""CREATE TABLE mutedmember (
+        await self.db.execute_query("""CREATE TABLE mutedmember (
             user_id BIGINT NOT NULL, 
             role_id BIGINT NOT NULL, 
             mute_ts BIGINT DEFAULT NULL, 
             muted_for_seconds BIGINT DEFAULT NULL,
             PRIMARY KEY (user_id, role_id)
             )""")
-        await db.commit()
-        await mycursor.close()
 
         return await ctx.send("**Table __MutedMember__ created!**", delete_after=3)
 
@@ -38,54 +35,35 @@ class ModerationMutedMemberTable(commands.Cog):
         if not await self.check_table_mutedmember_exists():
             return await ctx.send("**Table __MutedMember__ doesn't exist!**")
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("DROP TABLE mutedmember")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DROP TABLE mutedmember")
 
         return await ctx.send("**Table __MutedMember__ dropped!**", delete_after=3)
 
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
     async def reset_table_mutedmember(self, ctx):
-        '''
-        (ADM) Resets the MutedMember table.
-        '''
+        """ (ADM) Resets the MutedMember table. """
+
         if not await self.check_table_mutedmember_exists():
             return await ctx.send("**Table __MutedMember__ doesn't exist yet**")
 
         await ctx.message.delete()
-        mycursor, db = await the_database()
-        await mycursor.execute("DELETE FROM mutedmember")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DELETE FROM mutedmember")
 
         return await ctx.send("**Table __mutedmember__ reset!**", delete_after=3)
 
     async def check_table_mutedmember_exists(self) -> bool:
-        '''
-        Checks if the MutedMember table exists
-        '''
-        mycursor, db = await the_database()
-        await mycursor.execute("SHOW TABLE STATUS LIKE 'mutedmember'")
-        table_info = await mycursor.fetchall()
-        await mycursor.close()
+        """ Checks if the MutedMember table exists """
 
-        if len(table_info) == 0:
-            return False
-
-        else:
-            return True
+        return await self.db.table_exists("mutedmember")
 
     async def get_expired_tempmutes(self, current_ts: int) -> List[int]:
         """ Gets expired tempmutes.
         :param current_ts: The current timestamp. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("SELECT DISTINCT(user_id) FROM mutedmember WHERE (%s -  mute_ts) >= muted_for_seconds", (current_ts,))
-        tempmutes = list(map(lambda m: m[0], await mycursor.fetchall()))
-        await mycursor.close()
-        return tempmutes
+        tempmutes = await self.db.execute_query(
+            "SELECT DISTINCT(user_id) FROM mutedmember WHERE (%s -  mute_ts) >= muted_for_seconds", (current_ts,), fetch="all")
+        return list(map(lambda m: m[0], tempmutes))
 
     async def get_muted_members(self, current_ts: int, days_ago: Optional[int] = 0) -> List[int]:
         """ Gets muted members from the past X days.
@@ -93,61 +71,36 @@ class ModerationMutedMemberTable(commands.Cog):
         :param days_ago: The amount of days ago to get muted members from. [Optional] """
 
         seconds_ago = 86400 * days_ago
-        mycursor, db = await the_database()
-        await mycursor.execute("SELECT DISTINCT(user_id) FROM mutedmember WHERE (%s -  mute_ts) >= %s", (current_ts, seconds_ago))
-        muted_members = list(map(lambda m: m[0], await mycursor.fetchall()))
-        await mycursor.close()
-        return muted_members
-
+        muted_members = await self.db.execute_query(
+            "SELECT DISTINCT(user_id) FROM mutedmember WHERE (%s -  mute_ts) >= %s", (current_ts, seconds_ago), fetch="all")
+        return list(map(lambda m: m[0], muted_members))
 
     async def insert_in_muted(self, user_role_ids: List[Tuple[int]]):
-        mycursor, db = await the_database()
-        await mycursor.executemany(
+        await self.db.execute_query(
             """
             INSERT INTO mutedmember (
-            user_id, role_id, mute_ts, muted_for_seconds) VALUES (%s, %s, %s, %s)""", user_role_ids
+            user_id, role_id, mute_ts, muted_for_seconds) VALUES (%s, %s, %s, %s)""", user_role_ids, execute_many=True
             )
-        await db.commit()
-        await mycursor.close()
 
     async def get_muted_roles(self, user_id: int):
-        mycursor, db = await the_database()
-        await mycursor.execute("SELECT * FROM mutedmember WHERE user_id = %s", (user_id,))
-        user_roles = await mycursor.fetchall()
-        await mycursor.close()
-        return user_roles
+        return await self.db.execute_query("SELECT * FROM mutedmember WHERE user_id = %s", (user_id,), fetch="all")
 
     async def remove_role_from_system(self, user_role_ids: int):
-        mycursor, db = await the_database()
-        await mycursor.executemany("DELETE FROM mutedmember WHERE user_id = %s AND role_id = %s", user_role_ids)
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DELETE FROM mutedmember WHERE user_id = %s AND role_id = %s", user_role_ids, execute_many=True)
 
     async def remove_all_roles_from_system(self, user_id: int):
         """ Removes all muted-roles linked to a user from the system.
         :param user_id: The ID of the user. """
 
-        mycursor, db = await the_database()
-        await mycursor.executemany("DELETE FROM mutedmember WHERE user_id = %s", (user_id,))
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DELETE FROM mutedmember WHERE user_id = %s", (user_id,), execute_many=True)
 
     async def update_mute_time(self, user_id: int, current_time: int, time: int):
-        mycursor, db = await the_database()
-        await mycursor.execute("UPDATE mutedmember SET mute_ts = %s, muted_for_seconds = %s WHERE user_id = %s", (current_time, time, user_id))
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("UPDATE mutedmember SET mute_ts = %s, muted_for_seconds = %s WHERE user_id = %s", (current_time, time, user_id))
 
     async def get_mute_time(self, user_id: int):
-        mycursor, db = await the_database()
-        await mycursor.executemany("SELECT mute_ts, muted_for_seconds FROM mutedmember WHERE user_id = %s", (user_id,))
-        times = await mycursor.fetchone()
-        await mycursor.close()
-        return times
-    
+        return await self.db.execute_query(
+            "SELECT mute_ts, muted_for_seconds FROM mutedmember WHERE user_id = %s", (user_id,), execute_many=True, fetch="one")
+
     async def get_not_unmuted_members(self):
-        mycursor, db = await the_database()
-        await mycursor.execute("SELECT user_id, mute_ts FROM mutedmember WHERE muted_for_seconds IS NULL")
-        muted_members = await mycursor.fetchall()
-        await mycursor.close()
+        muted_members = await self.db.execute_query("SELECT user_id, mute_ts FROM mutedmember WHERE muted_for_seconds IS NULL", fetch="all")
         return set(muted_members)
