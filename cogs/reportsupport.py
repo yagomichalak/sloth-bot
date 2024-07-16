2eaac7f7d3a2f6f0a067c0dce5abd5fffdf6959a
Yago Michalak <56281335+yagomichalak@users.noreply.github.com>
2024-07-17 01:00:01 +0200

    Update reportsupport.py

--- cogs/reportsupport.py
+++ cogs/reportsupport.py
@@ -243,7 +243,7 @@ class ReportSupport(*report_support_classes):
                 await self.remove_user_open_channel(member.id)
 
         # Report someone
-        case_cat = discord.utils.get(guild.categories, id=case_cat_id)       
+        case_cat = discord.utils.get(guild.categories, id=case_cat_id)
         counter = await self.get_case_number()
         moderator = discord.utils.get(guild.roles, id=moderator_role_id)
         cosmos_role = discord.utils.get(guild.roles, id=self.cosmos_role_id)
@@ -298,7 +298,7 @@ class ReportSupport(*report_support_classes):
 
         if open_channel := await self.member_has_open_channel(member.id):
             if open_channel := discord.utils.get(guild.text_channels, id=open_channel[1]):
-                embed = discord.Embed(title="Error!", description=f"**You already have an open channel! ({open_channel.mention})**", color=discord.Color.red())         
+                embed = discord.Embed(title="Error!", description=f"**You already have an open channel! ({open_channel.mention})**", color=discord.Color.red())
                 await interaction.followup.send(embed=embed, ephemeral=True)
                 return False
             else:
@@ -366,8 +366,8 @@ class ReportSupport(*report_support_classes):
         member = interaction.user
         guild = interaction.guild
 
-        if open_channel := await self.member_has_open_channel(member.id):           
-            if open_channel := discord.utils.get(guild.text_channels, id=open_channel[1]):             
+        if open_channel := await self.member_has_open_channel(member.id):
+            if open_channel := discord.utils.get(guild.text_channels, id=open_channel[1]):
                 embed = discord.Embed(title="Error!", description=f"**You already have an open channel! ({open_channel.mention})**", color=discord.Color.red())
                 await interaction.followup.send(embed=embed, ephemeral=True)
                 return False
@@ -383,7 +383,7 @@ class ReportSupport(*report_support_classes):
             read_messages=True, send_messages=True, connect=False, view_channel=True),
         moderator: discord.PermissionOverwrite(
             read_messages=True, send_messages=True, connect=False, view_channel=True, manage_messages=True)}
-        try:  
+        try:
             the_channel = await guild.create_text_channel(name=f"{'-'.join(type_help.split())}", category=case_cat, overwrites=overwrites)
         except:
             await interaction.followup.send("**Something went wrong with it, please contact an admin!**", ephemeral=True)
