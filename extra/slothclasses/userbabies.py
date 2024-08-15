32bc2c60ce385da35053954ea1a27515004b33a2
Yago Michalak <yagomichalak@gmail.com>
2024-08-16 00:09:50 +0200

    refactor: Refactors the userbabies.py and userpets.py database methods

--- extra/slothclasses/userbabies.py
+++ extra/slothclasses/userbabies.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
-from mysqldb import the_database
+from mysqldb import DatabaseCore
 from typing import List, Union, Optional
 from extra import utils
+
 
 class UserBabiesTable(commands.Cog):
     """ Class for the UserBabies table and its commands and methods. """
@@ -11,6 +12,7 @@ class UserBabiesTable(commands.Cog):
         """ Class init method. """
 
         self.client = client
+        self.db = DatabaseCore()
 
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
@@ -21,8 +23,7 @@ class UserBabiesTable(commands.Cog):
         if await self.check_user_babies_table_exists():
             return await ctx.send(f"**The UserBabies table already exists, {member.mention}!**")
 
-        mycursor, db = await the_database()
-        await mycursor.execute("""CREATE TABLE UserBabies (
+        await self.db.execute_query("""CREATE TABLE UserBabies (
             parent_one BIGINT NOT NULL,
             parent_two BIGINT NOT NULL,
             baby_name VARCHAR(25) DEFAULT 'Embryo',
@@ -35,8 +36,6 @@ class UserBabiesTable(commands.Cog):
             PRIMARY KEY (parent_one, parent_two)
             ) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci
         """)
-        await db.commit()
-        await mycursor.close()
 
         await ctx.send(f"**`UserBabies` table created, {member.mention}!**")
 
@@ -49,10 +48,7 @@ class UserBabiesTable(commands.Cog):
         if not await self.check_user_babies_table_exists():
             return await ctx.send(f"**The UserBabies table doesn't exist, {member.mention}!**")
 
-        mycursor, db = await the_database()
-        await mycursor.execute("DROP TABLE UserBabies")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DROP TABLE UserBabies")
 
         await ctx.send(f"**`UserBabies` table dropped, {member.mention}!**")
 
@@ -65,24 +61,14 @@ class UserBabiesTable(commands.Cog):
         if not await self.check_user_babies_table_exists():
             return await ctx.send(f"**The UserBabies table doesn't exist yet, {member.mention}!**")
 
-        mycursor, db = await the_database()
-        await mycursor.execute("DELETE FROM UserBabies")
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("DELETE FROM UserBabies")
 
         await ctx.send(f"**`UserBabies` table reset, {member.mention}!**")
 
     async def check_user_babies_table_exists(self) -> bool:
         """ Checks whether the UserBabies table exists in the database. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SHOW TABLE STATUS LIKE 'UserBabies'")
-        exists = await mycursor.fetchone()
-        await mycursor.close()
-        if exists:
-            return True
-        else:
-            return False
+        return await self.db.table_exists("UserBabies")
 
     async def insert_user_baby(self, 
         parent_one: int, parent_two: int, 
@@ -96,68 +82,47 @@ class UserBabiesTable(commands.Cog):
         current_ts = await utils.get_timestamp()
         other_ts = current_ts + 3600
 
-        mycursor, db = await the_database()
         if baby_name and baby_class:
-            await mycursor.execute("""
+            await self.db.execute_query("""
                 INSERT INTO UserBabies (
                     parent_one, parent_two, baby_name, baby_class, life_points_ts, food_ts, birth_ts
                 ) VALUES (%s, %s, %s, %s, %s, %s, %s)""", (parent_one, parent_two, baby_name, baby_class, other_ts, other_ts, current_ts))
         else:
-            await mycursor.execute("""
+            await self.db.execute_query("""
                 INSERT INTO UserBabies (
                     parent_one, parent_two, life_points_ts, food_ts, birth_ts
                 ) VALUES (%s, %s, %s, %s, %s)""", (parent_one, parent_two, other_ts, other_ts, current_ts))
-        await db.commit()
-        await mycursor.close()
 
     async def get_user_baby(self, parent_id: int) -> List[Union[str, int]]:
         """ Get the user's baby.
         :param parent_id: The ID of one of the baby's parents. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SELECT * FROM UserBabies WHERE parent_one = %s OR parent_two = %s", (parent_id, parent_id))
-        user_baby = await mycursor.fetchone()
-        await mycursor.close()
-        return user_baby
+        return await self.db.execute_query("SELECT * FROM UserBabies WHERE parent_one = %s OR parent_two = %s", (parent_id, parent_id), fetch="one")
 
     async def get_babies(self) -> List[List[Union[str, int]]]:
         """ Get all user babies. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SELECT * FROM UserBabies")
-        user_babies = await mycursor.fetchall()
-        await mycursor.close()
-        return user_babies
+        return await self.db.execute_query("SELECT * FROM UserBabies", fetch="all")
 
     async def get_hungry_babies(self, current_ts: int) -> List[List[Union[str, int]]]:
         """ Get all user hungry babies.
         :param current_ts: The current timestamp. """
 
-        mycursor, _ = await the_database()
-        await mycursor.execute("SELECT * FROM UserBabies WHERE %s - food_ts >= 7200", (current_ts,))
-        user_babies = await mycursor.fetchall()
-        await mycursor.close()
-        return user_babies
+        return await self.db.execute_query("SELECT * FROM UserBabies WHERE %s - food_ts >= 7200", (current_ts,), fetch="all")
 
     async def update_user_baby_name(self, parent_id: int, baby_name: str) -> None:
         """ Updates the User Baby's name.
         :param parent_id: The ID of one of the baby's parents.
         :param baby_name: The new baby name to update to. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("UPDATE UserBabies SET baby_name = %s WHERE parent_one = %s OR parent_two = %s", (baby_name, parent_id, parent_id))
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("UPDATE UserBabies SET baby_name = %s WHERE parent_one = %s OR parent_two = %s", (baby_name, parent_id, parent_id))
 
     async def update_user_baby_class(self, parent_id: int, baby_class: str) -> None:
         """ Updates the User Baby's class.
         :param parent_id: The ID of one of the the baby's parents.
         :param baby_class: The new baby class to update to. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("UPDATE UserBabies SET baby_class = %s WHERE parent_one = %s OR parent_two = %s", (baby_class, parent_id, parent_id))
-        await db.commit()
-        await mycursor.close()
+        await self.db.execute_query("UPDATE UserBabies SET baby_class = %s WHERE parent_one = %s OR parent_two = %s", (baby_class, parent_id, parent_id))
 
     async def update_user_baby_lp(self, parent_id: int, increment: int = 5, current_ts: Optional[int] = None) -> None:
         """ Updates the User Baby's life points.
@@ -165,14 +130,11 @@ class UserBabiesTable(commands.Cog):
         :param increment: The incremention value to apply. [Default = 5] (Can be negative)
         :param current_ts: The current timestamp. [Optional] """
 
-        mycursor, db = await the_database()
         if current_ts:
-            await mycursor.execute("""UPDATE UserBabies SET life_points = life_points + %s, life_points_ts = %s WHERE parent_one = %s OR parent_two = %s
+            await self.db.execute_query("""UPDATE UserBabies SET life_points = life_points + %s, life_points_ts = %s WHERE parent_one = %s OR parent_two = %s
             """, (increment, current_ts, parent_id, parent_id))
         else:
-            await mycursor.execute("UPDATE UserBabies SET life_points = life_points + %s WHERE parent_one = %s OR parent_two = %s", (increment, parent_id, parent_id))
-        await db.commit()
-        await mycursor.close()
+            await self.db.execute_query("UPDATE UserBabies SET life_points = life_points + %s WHERE parent_one = %s OR parent_two = %s", (increment, parent_id, parent_id))
 
     async def update_user_baby_food(self, parent_id: int, increment: int = 5, current_ts: Optional[int] = None) -> None:
         """ Updates the User Baby's food status.
@@ -180,21 +142,14 @@ class UserBabiesTable(commands.Cog):
         :param increment: The incremention value to apply. [Default = 5] (Can be negative)
         :param current_ts: The current timestamp. [Optional] """
 
-        mycursor, db = await the_database()
         if current_ts:
-            await mycursor.execute("""UPDATE UserBabies SET food = food + %s, food_ts = %s WHERE parent_one = %s OR parent_two = %s
+            await self.db.execute_query("""UPDATE UserBabies SET food = food + %s, food_ts = %s WHERE parent_one = %s OR parent_two = %s
             """, (increment, current_ts, parent_id, parent_id))
         else:
-            await mycursor.execute("UPDATE UserBabies SET food = food + %s WHERE parent_one = %s OR parent_two = %s", (increment, parent_id, parent_id))
-        await db.commit()
-        await mycursor.close()
+            await self.db.execute_query("UPDATE UserBabies SET food = food + %s WHERE parent_one = %s OR parent_two = %s", (increment, parent_id, parent_id))
 
     async def delete_user_baby(self, parent_id: int) -> None:
         """ Deletes the user's baby.
         :param parent_id: The ID of one of the baby's parents. """
 
-        mycursor, db = await the_database()
-        await mycursor.execute("DELETE FROM UserBabies WHERE parent_one = %s or parent_two = %s", (parent_id, parent_id))
-        await db.commit()
-        await mycursor.close()
-
+        await self.db.execute_query("DELETE FROM UserBabies WHERE parent_one = %s or parent_two = %s", (parent_id, parent_id))
