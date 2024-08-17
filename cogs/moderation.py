22e7f09ebe4fa926606c57cfeffeff61d501bd2a
hodjapie <hodjaone@gmail.com>
2024-08-18 01:44:05 +0300

    fix: watchlist edits sending users an edited reason dm
    also includes a slight label change for one of the buttons in z!ui

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -2105,7 +2105,8 @@ We appreciate your understanding and look forward to hearing from you. """, embe
                 await moderation_log.send(embed=embed)
 
                 try:
-                    await member.send(embed=general_embed)
+                    if user_infraction[0][1] != "watchlist":
+                        await member.send(embed=general_embed)
                 except Exception:
                     pass
 
